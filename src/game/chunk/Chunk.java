7e23fa8f689d7d6ee215c526c975b4567cb7fc34
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-04-26 21:20:33 -0400

    "Fix" black flash when mining

--- src/game/chunk/Chunk.java
+++ src/game/chunk/Chunk.java
@@ -314,8 +314,9 @@ public class Chunk {
         }
         lightFloodFill(x, y, z);
         thisChunk.modified = true;
-        generateChunkMesh(chunkX,chunkZ,yPillar);//instant update
-        instantUpdateNeighbor(chunkX, chunkZ,blockX,y,blockZ);//instant update
+
+        //generateChunkMesh(chunkX,chunkZ,yPillar);//instant update
+        //instantUpdateNeighbor(chunkX, chunkZ,blockX,y,blockZ);//instant update
     }
 
     public static void placeBlock(int x,int y,int z, int ID, int rot){
