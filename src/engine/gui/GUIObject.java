2e476dea74b9c1cf67f68e5b4eab4fea7a7fa024
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-05-20 03:56:40 -0400

    Create shadowed text factory

--- src/engine/gui/GUIObject.java
+++ src/engine/gui/GUIObject.java
@@ -4,17 +4,21 @@ import engine.graph.Mesh;
 import org.joml.Vector2d;
 import org.joml.Vector2f;
 
-import static engine.gui.GUI.getButtonMesh;
-import static engine.gui.TextHandling.createTextCentered;
-import static engine.gui.TextHandling.translateCharToArray;
+import static engine.gui.TextHandling.*;
 
 public class GUIObject {
+    /*
+    types:
+    0 - button
+    1 - slider bar
+     */
     public byte type;
     public Mesh textMesh;
     public Vector2d pos = new Vector2d();
     public Vector2f buttonScale = new Vector2f();
+    public boolean selected;
 
-    //initializer for button
+    //initializer for button (auto scaled width)
     public GUIObject(String text, Vector2d pos){
         this.textMesh = createTextCentered(text, 1,1,1);
         float totalLengthReal = 0;
@@ -23,14 +27,21 @@ public class GUIObject {
             float[] thisCharacterArray = translateCharToArray(letter);
             totalLengthReal += thisCharacterArray[4] + 0.1f;
         }
+        totalLengthReal -= 0.1f;
         this.buttonScale.x = totalLengthReal/2f;
         this.pos = pos;
+        this.type = 0;
+        this.selected = false;
     }
 
     //initializer for button with fixed width
     public GUIObject(String text, Vector2d pos, float width){
-        this.textMesh = createTextCentered(text, 1,1,1);
+        this.textMesh = createTextCenteredWithShadow(text, 1,1,1);
         this.buttonScale.x = width;
         this.pos = pos;
+        this.type = 0;
+        this.selected = false;
     }
+
+
 }
