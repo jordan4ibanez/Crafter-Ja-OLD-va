bffe21c9e0d8bd50666463e1b193e681b1bcb04c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-05-23 04:07:23 -0400

    Improve performance of posToIndex and indexToPos

--- src/game/chunk/ChunkMath.java
+++ src/game/chunk/ChunkMath.java
@@ -9,16 +9,17 @@ public class ChunkMath {
 
     private final static int xMax = 16;
     private final static int yMax = 128;
+    private final static int length = xMax * yMax;
 
     public static int posToIndex( int x, int y, int z ) {
-        return (z * xMax * yMax) + (y * xMax) + x;
+        return (z * length) + (y * xMax) + x;
     }
 
     //keep this for if it's needed in the future
     //for ABMs or something
     public static Vector3i indexToPos( int i ) {
-        final int z = i / (xMax * yMax);
-        i -= (z * xMax * yMax);
+        final int z = i / length;
+        i -= (z * length);
         final int y = i / xMax;
         final int x = i % xMax;
         return new Vector3i( x, y, z);
