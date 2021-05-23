cac7f09e9ba35be90097c721ea58c8456d97a958
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-05-23 04:15:39 -0400

    Swap out FreeSound for OpenAL credits, for now

--- src/engine/credits/Credits.java
+++ src/engine/credits/Credits.java
@@ -19,7 +19,7 @@ public class Credits {
         creditParts[1] = create2DMesh(8.4375f,1f,"textures/minetest_logo.png");
         creditParts[2] = create2DMesh(9.34782608696f,1f,"textures/mineclone2.png");
 
-        creditParts[3] = createTextCenteredWithShadow("Lead Programmer: jordan4ibanez", 1,1,1);
+        creditParts[3] = createTextCenteredWithShadow("Programmer: jordan4ibanez", 1,1,1);
         creditParts[4] = create2DMesh(2f,2f,"textures/jordan4ibanez.png");
 
         creditParts[5] = createTextCenteredWithShadow("Textures: gerold55", 1,1,1);
@@ -31,11 +31,11 @@ public class Credits {
         creditParts[9] = createTextCenteredWithShadow("Title Music: Dark Reaven Music", 1,1,1);
         creditParts[10] = create2DMesh(2f,2f,"textures/dark_reaven.png");
 
-        creditParts[11] = createTextCenteredWithShadow("Sounds: FreeSound.org", 1,1,1);
-        creditParts[12] = create2DMesh(4.06349206349f,1f,"textures/freesound.png");
+        creditParts[11] = createTextCenteredWithShadow("Java Library: LWJGL", 1,1,1);
+        creditParts[12] = create2DMesh(2f,2f,"textures/lwjgl.png");
 
-        creditParts[13] = createTextCenteredWithShadow("Java Library: LWJGL", 1,1,1);
-        creditParts[14] = create2DMesh(2f,2f,"textures/lwjgl.png");
+        creditParts[13] = createTextCenteredWithShadow("Sound Library: OpenAL", 1,1,1);
+        creditParts[14] = create2DMesh(1.84049079755f,1f,"textures/openal.png");
 
         creditParts[15] = createTextCenteredWithShadow("OpenGL Library: GLFW", 1,1,1);
         creditParts[16] = create2DMesh(2f,2f,"textures/glfw.png");
