bedc430c941012d919f1e83ef07d4500bc11f830
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-05-23 11:57:56 -0400

    Add in fixed X and Y position textWithShadow creation

--- src/engine/gui/TextHandling.java
+++ src/engine/gui/TextHandling.java
@@ -754,8 +754,8 @@ public class TextHandling {
 
 
         //reset x position like a type writer
-        x = -totalLengthReal/2f + 0.05f;
-        float y = -0.05f;
+        x = -totalLengthReal/2f + 0.075f;
+        float y = -0.075f;
 
         //shadow
         for (char letter : text.toCharArray()) {
@@ -868,6 +868,155 @@ public class TextHandling {
                 light[a    ] = (r);
                 light[a + 1] = (g);
                 light[a + 2] = (b);
+                a += 3;
+            }
+
+            indices[t    ] = (0 + indicesCount);
+            indices[t + 1] = (1 + indicesCount);
+            indices[t + 2] = (2 + indicesCount);
+            indices[t + 3] = (0 + indicesCount);
+            indices[t + 4] = (2 + indicesCount);
+            indices[t + 5] = (3 + indicesCount);
+
+            t += 6;
+            indicesCount += 4;
+
+
+            textureCoord[w    ] = (thisCharacterArray[1]);
+            textureCoord[w + 1] = (thisCharacterArray[2]);
+            textureCoord[w + 2] = (thisCharacterArray[0]);
+            textureCoord[w + 3] = (thisCharacterArray[2]);
+            textureCoord[w + 4] = (thisCharacterArray[0]);
+            textureCoord[w + 5] = (thisCharacterArray[3]);
+            textureCoord[w + 6] = (thisCharacterArray[1]);
+            textureCoord[w + 7] = (thisCharacterArray[3]);
+            w += 8;
+
+            //shift the left of the letter to the right
+            //kind of like a type writer
+
+            //make this use the 4th float array variable to space properly
+            //add 0.1f so that characters are not squished together
+
+            x += thisCharacterArray[4] + 0.1f;
+        }
+
+        return new Mesh(positions, light, indices, textureCoord, fontTextureAtlas);
+    }
+
+    public static Mesh createTextWithShadow(String text, float r, float g, float b){
+
+        //x is the actual position in the mesh creation of the letter
+        //divide the actual length before it's created, start the typewriter
+        //half way across
+        float x = 0;
+
+        //get the amount of letters in the string
+        int stringLength = text.length();
+
+        float[] positions = new float[stringLength * 12 * 2];
+        float[] textureCoord = new float[stringLength * 8 * 2];
+        int[] indices = new int[stringLength * 6 * 2];
+        float[] light = new float[stringLength * 12 * 2];
+
+        int indicesCount = 0;
+        int i = 0; //positions count
+        int a = 0; //light count
+        int w = 0; //textureCoord count
+        int t = 0; //indices count
+
+
+        //foreground
+        for (char letter : text.toCharArray()) {
+
+            //translate the character (char primitive) into a usable float array
+            float[] thisCharacterArray = translateCharToArray(letter);
+
+            positions[i     ] = (x + thisCharacterArray[4]);
+            positions[i + 1 ] = (0.5f);
+            positions[i + 2 ] = (0f);
+
+            positions[i + 3 ] = (x);
+            positions[i + 4 ] = (0.5f);
+            positions[i + 5 ] = (0f);
+
+            positions[i + 6 ] = (x);
+            positions[i + 7 ] = (-0.5f);
+            positions[i + 8 ] = (0f);
+
+            positions[i + 9 ] = (x + thisCharacterArray[4]);
+            positions[i + 10] = (-0.5f);
+            positions[i + 11] = (0f);
+            i += 12;
+
+            for (int q = 0; q < 4; q++) {
+                light[a    ] = (r);
+                light[a + 1] = (g);
+                light[a + 2] = (b);
+                a += 3;
+            }
+
+            indices[t    ] = (0 + indicesCount);
+            indices[t + 1] = (1 + indicesCount);
+            indices[t + 2] = (2 + indicesCount);
+            indices[t + 3] = (0 + indicesCount);
+            indices[t + 4] = (2 + indicesCount);
+            indices[t + 5] = (3 + indicesCount);
+
+            t += 6;
+            indicesCount += 4;
+
+
+            textureCoord[w    ] = (thisCharacterArray[1]);
+            textureCoord[w + 1] = (thisCharacterArray[2]);
+            textureCoord[w + 2] = (thisCharacterArray[0]);
+            textureCoord[w + 3] = (thisCharacterArray[2]);
+            textureCoord[w + 4] = (thisCharacterArray[0]);
+            textureCoord[w + 5] = (thisCharacterArray[3]);
+            textureCoord[w + 6] = (thisCharacterArray[1]);
+            textureCoord[w + 7] = (thisCharacterArray[3]);
+            w += 8;
+
+            //shift the left of the letter to the right
+            //kind of like a type writer
+
+            //make this use the 4th float array variable to space properly
+            //add 0.1f so that characters are not squished together
+
+            x += thisCharacterArray[4] + 0.1f;
+        }
+
+
+        //reset x position like a type writer
+        x = 0.075f;
+        float y = -0.075f;
+
+        //shadow
+        for (char letter : text.toCharArray()) {
+            //translate the character (char primitive) into a usable float array
+            float[] thisCharacterArray = translateCharToArray(letter);
+
+            positions[i     ] = (x + thisCharacterArray[4]);
+            positions[i + 1 ] = (0.5f + y);
+            positions[i + 2 ] = (0f);
+
+            positions[i + 3 ] = (x);
+            positions[i + 4 ] = (0.5f + y);
+            positions[i + 5 ] = (0f);
+
+            positions[i + 6 ] = (x);
+            positions[i + 7 ] = (-0.5f + y);
+            positions[i + 8 ] = (0f);
+
+            positions[i + 9 ] = (x + thisCharacterArray[4]);
+            positions[i + 10] = (-0.5f + y);
+            positions[i + 11] = (0f);
+            i += 12;
+
+            for (int q = 0; q < 4; q++) {
+                light[a    ] = (0);
+                light[a + 1] = (0);
+                light[a + 2] = (0);
                 a += 3;
             }
 
