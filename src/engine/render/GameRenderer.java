2ac437b9f7c97d21e1b06fc0b4f35c10df5976f7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-05-24 06:02:31 -0400

    Simplify getChunkDistanceFromPlayer in GameRenderer

--- src/engine/render/GameRenderer.java
+++ src/engine/render/GameRenderer.java
@@ -909,12 +909,6 @@ public class GameRenderer {
 
     private static double getChunkDistanceFromPlayer(int x, int z){
         Vector3i currentChunk = getPlayerCurrentChunk();
-
-        //x distance
-        double distanceX = getDistance(currentChunk.x,0,0, x, 0, 0);
-        //z distance
-        double distanceZ = getDistance(0,0,currentChunk.z, 0, 0, z);
-
-        return Math.max(distanceZ, distanceX);
+        return Math.max(getDistance(0,0,currentChunk.z, 0, 0, z), getDistance(currentChunk.x,0,0, x, 0, 0));
     }
 }
