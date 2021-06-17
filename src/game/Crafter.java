a2621cc2b187f4745a350736c10cadf339b66014
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-06-17 02:31:08 -0400

    Bump version to 0.04b

--- src/game/Crafter.java
+++ src/game/Crafter.java
@@ -34,7 +34,7 @@ import static game.player.Player.*;
 public class Crafter {
 
     //fields
-    private static final String versionName = "Crafter 0.04a Survival Test";
+    private static final String versionName = "Crafter 0.04b Survival Test";
 
     public static String getVersionName(){
         return versionName;
