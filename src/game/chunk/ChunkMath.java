5abddb91aa28b32642af28c34e351b78091d0133
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-06-21 23:46:16 -0400

    Hardcode ChunkMath to keep values on the stack

--- src/game/chunk/ChunkMath.java
+++ src/game/chunk/ChunkMath.java
@@ -6,21 +6,21 @@ import org.joml.Vector3i;
 //ported from C++ to Java
 public class ChunkMath {
 
-    private final static int xMax = 16;
-    private final static int yMax = 128;
-    private final static int length = xMax * yMax;
+    //private final static int xMax = 16;
+    //private final static int yMax = 128;
+    //private final static int length = xMax * yMax; // 2048
 
     public static int posToIndex( int x, int y, int z ) {
-        return (z * length) + (y * xMax) + x;
+        return (z * 2048) + (y * 16) + x;
     }
 
     //keep this for if it's needed in the future
     //for ABMs or something
     public static Vector3i indexToPos( int i ) {
-        final int z = i / length;
-        i -= (z * length);
-        final int y = i / xMax;
-        final int x = i % xMax;
+        final int z = i / 2048;
+        i -= (z * 2048);
+        final int y = i / 16;
+        final int x = i % 16;
         return new Vector3i( x, y, z);
     }
 }
