3367d7dc4d4c96d5613e7d3416266af05ef77276
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-06-22 04:39:30 -0400

    Add basics of TimeOfDay

--- src/engine/time/TimeOfDay.java
+++ src/engine/time/TimeOfDay.java
@@ -1,5 +1,35 @@
 package engine.time;
 
-public class TimeOfDay {
+import static engine.time.Time.getDelta;
 
+public class TimeOfDay {
+    private static double timeOfDay = 0d;
+
+    private static double timeSpeed = 72d;
+
+    public static void tickUpTimeOfDay(){
+        timeOfDay += getDelta() * timeSpeed;
+
+        if (timeOfDay >= 24000){
+            timeOfDay -= 24000;
+        }
+
+        System.out.println(timeOfDay);
+    }
+
+    public static double getTimeOfDay(){
+        return timeOfDay;
+    }
+
+    public static double getTimeSpeed(){
+        return timeSpeed;
+    }
+
+    public static void setTimeOfDay(double newTime){
+        timeOfDay = newTime;
+    }
+
+    public static void setTimeSpeed(double newSpeed){
+        timeSpeed = newSpeed;
+    }
 }
