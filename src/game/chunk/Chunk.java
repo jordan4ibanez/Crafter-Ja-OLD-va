d5174ac0695b6c412b46c11d9002f384351bed12
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-06-24 08:18:27 -0400

    Update Chunk.java

--- src/game/chunk/Chunk.java
+++ src/game/chunk/Chunk.java
@@ -33,9 +33,9 @@ import static game.player.Player.*;
 
 public class Chunk {
 
-    //todo: this is an experiment, if all of a sudden stuff starts crashing, this is why - this is disabled for now
-    private static final Object2ObjectOpenHashMap<Vector2i, ChunkObject> map = new Object2ObjectOpenHashMap<>();
-    //private static final ConcurrentHashMap<Vector2i, ChunkObject> map = new ConcurrentHashMap<>();
+    //DO NOT CHANGE THE DATA CONTAINER - but this is left here for people to experiment with
+    //private static final Object2ObjectOpenHashMap<Vector2i, ChunkObject> map = new Object2ObjectOpenHashMap<>();
+    private static final ConcurrentHashMap<Vector2i, ChunkObject> map = new ConcurrentHashMap<>();
 
     public static ChunkObject[] getMap(){
         return map.values().toArray(new ChunkObject[0]);
