03000861a1f78eaad3d3bba1f6f70f608258c866
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-06-25 09:11:35 -0400

    Add mob spawning default interface

--- src/game/mob/MobInterface.java
+++ src/game/mob/MobInterface.java
@@ -1,6 +1,11 @@
 package game.mob;
 
 public interface MobInterface {
+    //what a mob does each tick
     default void onTick(MobObject thisObject){
+    }
+    //thrown into the mob spawning algorithm
+    default boolean spawnCalculation(){
+        return false;
     }
 }
