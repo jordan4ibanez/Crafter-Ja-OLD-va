7feb71b6329653d40dd6967e06b25b60dabb3ceb
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-06-26 04:58:46 -0400

    Update TimeOfDay.java

--- src/engine/time/TimeOfDay.java
+++ src/engine/time/TimeOfDay.java
@@ -10,7 +10,7 @@ public class TimeOfDay {
 
     private static final double dayCompletion = 86_400d;
     private static double timeOfDay = 21_600d; //6AM 0600 Hours //21_600d
-    private static double timeSpeed = 7200d; //following Minetest wiki - 72 times faster (following time_speed in minetest.conf)
+    private static double timeSpeed = 72d; //following Minetest wiki - 72 times faster (following time_speed in minetest.conf)
 
     public static void tickUpTimeOfDay(){
         timeOfDay += getDelta() * timeSpeed; //this calculation was ridiculous to get
