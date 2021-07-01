77c038f4961c372a91cfe8b1596e065775697bbd
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-07-01 12:40:23 -0400

    Introduce MicroFloatArray and MicroIntArray

---
+++ src/engine/highPerformanceContainers/MicroFloatArray.java
@@ -0,0 +1,88 @@
+package engine.highPerformanceContainers;
+
+public class MicroFloatArray {
+    private final int growthRate;
+
+    float[] dataContainer;
+
+    int currentPos = 0;
+    int maxSize;
+
+    public MicroFloatArray(int growthRate){
+        this.growthRate = growthRate;
+        dataContainer = new float[growthRate];
+        maxSize = growthRate;
+    }
+
+
+    //this is old delete it
+    private void put(float data){
+        if (currentPos == maxSize){
+            grow();
+        }
+        dataContainer[currentPos] = data;
+        currentPos++;
+    }
+
+    //for positions
+    public void pack(float a, float b, float c, float d, float e, float f, float g, float h, float i, float j, float k, float l){
+        put(a);
+        put(b);
+        put(c);
+        put(d);
+        put(e);
+        put(f);
+        put(g);
+        put(h);
+        put(i);
+        put(j);
+        put(k);
+        put(l);
+    }
+
+    //for texture coord
+    public void pack(float a, float b, float c, float d, float e, float f, float g, float h){
+        put(a);
+        put(b);
+        put(c);
+        put(d);
+        put(e);
+        put(f);
+        put(g);
+        put(h);
+    }
+
+    //for light data
+    public void pack(float a){
+        for (byte i = 0; i < 12; i++){
+            put(a);
+        }
+    }
+
+
+
+    public int size(){
+        return currentPos;
+    }
+
+    private void grow(){
+        float[] newContainer = new float[maxSize + growthRate];
+
+        if (maxSize >= 0) System.arraycopy(dataContainer, 0, newContainer, 0, maxSize);
+
+        dataContainer = null; //send to GC
+        dataContainer = newContainer;
+
+        maxSize += growthRate;
+    }
+
+    public float[] values(){
+        float[] returningContainer = new float[currentPos];
+        System.arraycopy(dataContainer, 0, returningContainer, 0, currentPos);
+        return returningContainer;
+    }
+
+    public void clear(){
+        dataContainer = null;
+    }
+}
---
+++ src/engine/highPerformanceContainers/MicroIntArray.java
@@ -0,0 +1,60 @@
+package engine.highPerformanceContainers;
+
+public class MicroIntArray {
+    private final int growthRate;
+
+    int[] dataContainer;
+
+    int currentPos = 0;
+    int maxSize;
+
+    public MicroIntArray(int growthRate){
+        this.growthRate = growthRate;
+        dataContainer = new int[growthRate];
+        maxSize = growthRate;
+    }
+
+    private void put(int data){
+        if (currentPos == maxSize){
+            grow();
+        }
+        dataContainer[currentPos] = data;
+        currentPos++;
+    }
+
+    //for the indices
+    public void pack(int a, int b, int c, int d, int e, int f) {
+        put(a);
+        put(b);
+        put(c);
+        put(d);
+        put(e);
+        put(f);
+    }
+
+    public int size(){
+        return currentPos;
+    }
+
+    private void grow(){
+        int[] newContainer = new int[maxSize + growthRate];
+
+        if (maxSize >= 0) System.arraycopy(dataContainer, 0, newContainer, 0, maxSize);
+
+        dataContainer = null; //send to GC
+        dataContainer = newContainer;
+
+        maxSize += growthRate;
+    }
+
+
+    public int[] values(){
+        int[] returningContainer = new int[currentPos];
+        System.arraycopy(dataContainer, 0, returningContainer, 0, currentPos);
+        return returningContainer;
+    }
+
+    public void clear(){
+        dataContainer = null;
+    }
+}
