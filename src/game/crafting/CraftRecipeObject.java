9b494865217dc62737d75426a7bd0dfdcf70d807
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-07-03 00:30:53 -0400

    Update CraftRecipeObject.java

--- src/game/crafting/CraftRecipeObject.java
+++ src/game/crafting/CraftRecipeObject.java
@@ -1,21 +1,11 @@
 package game.crafting;
 
-import org.joml.Vector2d;
-import org.joml.Vector2i;
-
 public class CraftRecipeObject {
     String[][]recipe;
     String output;
     int amountOutput;
 
     public CraftRecipeObject(String[][]recipe, String output, int amountOutput){
-        /*
-        String[][] flippedArray = new String[size.x][size.y];
-
-        for (int x = 0; x < size.x; x++){
-            flippedArray[y][x] = recipe[x][y];
-        }
-         */
         this.recipe = recipe;
         this.output = output;
         this.amountOutput = amountOutput;
