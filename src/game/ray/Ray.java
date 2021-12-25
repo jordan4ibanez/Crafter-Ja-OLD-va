5d57dece310a7cbddad76008b705c15814cf5fe6
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-12-24 19:51:55 -0500

    Fix new objects getting created every raycast

--- src/game/ray/Ray.java
+++ src/game/ray/Ray.java
@@ -23,11 +23,11 @@ import static game.player.Player.*;
 
 public class Ray {
 
-    private final static Vector3i finalPos = new Vector3i();
-    private final static Vector3d newPos   = new Vector3d();
-    private final static Vector3d realNewPos = new Vector3d();
-    private final static Vector3d lastPos  = new Vector3d();
-    private final static Vector3d cachePos = new Vector3d();
+    private static final Vector3i finalPos = new Vector3i();
+    private static final Vector3d newPos   = new Vector3d();
+    private static final Vector3d realNewPos = new Vector3d();
+    private static final Vector3d lastPos  = new Vector3d();
+    private static final Vector3d cachePos = new Vector3d();
     private static final Vector3i pointedThingAbove = new Vector3i();
 
     public static void playerRayCast(Vector3d pos, Vector3f dir, float length, boolean mining, boolean placing, boolean hasMined) {
@@ -37,6 +37,9 @@ public class Ray {
         byte foundBlock = -1;
 
         MobObject foundMob = null;
+
+        //get all mobs once
+        MobObject[] mobs = getAllMobs();
 
         for(double step = 0d; step <= length ; step += 0.001d) {
 
@@ -52,13 +55,14 @@ public class Ray {
             realNewPos.y = pos.y + cachePos.y;
             realNewPos.z = pos.z + cachePos.z;
 
-            MobObject[] mobs = getAllMobs();
-
 
             for (MobObject thisMob : mobs){
+
+                //null pointer catch
                 if (thisMob == null){
                     continue;
                 }
+
                 if (thisMob.pos.distance(realNewPos) <= 4.5 && thisMob.health > 0){
                     setPointAABB(thisMob.pos.x, thisMob.pos.y, thisMob.pos.z, thisMob.width,thisMob.height);
                     if(pointIsWithin(realNewPos.x, realNewPos.y, realNewPos.z)){
@@ -68,10 +72,12 @@ public class Ray {
                 }
             }
 
+            //pointing to a mob, break mob detection loop
             if (foundMob != null){
                 break;
             }
 
+            //do not get block when the floored (integer) position is the same as the last floored position
             //stop wasting cpu resources
             if (!newPos.equals(lastPos)) {
                 foundBlock = getBlock((int) newPos.x, (int) newPos.y, (int) newPos.z);
@@ -88,11 +94,17 @@ public class Ray {
             lastPos.set(newPos);
         }
 
+
+
+        //if a mob is found, the pointer is pointing to that thing
+        //this needs a rework
         if (foundMob != null){
+            //punch mob if pointing to it
             if (mining) {
                 punchMob(foundMob);
             }
             setPlayerWorldSelectionPos(null);
+            System.out.println("Found a mob :)");
         } else {
             if (foundBlock > 0 && isBlockPointable(foundBlock)) {
                 if (mining && hasMined) {
@@ -102,27 +114,28 @@ public class Ray {
                     //todo: make this call on punched
                     if (!isPlayerSneaking() && blockHasOnRightClickCall(foundBlock)) {
                         getBlockModifier(foundBlock).onRightClick(finalPos);
+                        System.out.println("stage 1");
                     } else {
                         Item wielding = getItemInInventorySlot(getPlayerInventorySelection(), 0);
-
-                        // THIS CREATES A NEW OBJECT IN HEAP!
-                        if (wielding != null && !wouldCollidePlacing(getPlayerPos(),getPlayerWidth(), getPlayerHeight(), new Vector3i(pointedThingAbove.x, pointedThingAbove.y, pointedThingAbove.z), wielding.definition.blockID, getPlayerDir()) && getItemInInventorySlot(getPlayerInventorySelection(), 0) != null && !getItemInInventorySlot(getPlayerInventorySelection(), 0).definition.isItem) {
+                        if (wielding != null && !wouldCollidePlacing(getPlayerPos(),getPlayerWidth(), getPlayerHeight(), pointedThingAbove, wielding.definition.blockID, getPlayerDir()) && getItemInInventorySlot(getPlayerInventorySelection(), 0) != null && !getItemInInventorySlot(getPlayerInventorySelection(), 0).definition.isItem) {
                             rayPlaceBlock(getItemInInventorySlot(getPlayerInventorySelection(), 0).definition.blockID);
+                            System.out.println("stage 2");
                         } else if (wielding != null && wielding.definition.isItem) {
                             if (getItemModifier(getItemInInventorySlot(getPlayerInventorySelection(), 0).name) != null) {
-                                // THIS CREATES A NEW OBJECT IN HEAP!
-                                getItemModifier(getItemInInventorySlot(getPlayerInventorySelection(), 0).name).onPlace(new Vector3i(finalPos.x, finalPos.y, finalPos.z), pointedThingAbove);
+                                getItemModifier(getItemInInventorySlot(getPlayerInventorySelection(), 0).name).onPlace(finalPos, pointedThingAbove);
+                                System.out.println("stage 3");
                             }
                         } else {
                             System.out.println("test3: This is a test of last branch of on place call");
                         }
                     }
                 } else {
-                    // THIS CREATES A NEW OBJECT IN HEAP!
-                    setPlayerWorldSelectionPos(new Vector3i(finalPos.x, finalPos.y, finalPos.z));
+                    setPlayerWorldSelectionPos(finalPos);
+                    System.out.println(finalPos.x + " " + finalPos.y + " " + finalPos.z);
                 }
             } else {
                 setPlayerWorldSelectionPos(null);
+                System.out.println("stage 5");
             }
         }
     }
