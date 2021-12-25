73887d408213ee991f6f50a412868cbb97ba87af
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-12-24 20:08:30 -0500

    Rework ParticleObject constructors to not entangle pointers

--- src/game/particle/ParticleObject.java
+++ src/game/particle/ParticleObject.java
@@ -7,7 +7,7 @@ import org.joml.Vector3i;
 
 public class ParticleObject {
     public final Vector3d pos = new Vector3d();
-    public final Vector3i oldFlooredPos;
+    public final Vector3i oldFlooredPos = new Vector3i();
     public final Vector3f inertia = new Vector3f();
     public final Mesh mesh;
     public float light;
@@ -18,7 +18,7 @@ public class ParticleObject {
     public ParticleObject(Vector3d pos, Vector3f inertia, Mesh mesh, int key){
         this.pos.set(pos.x,pos.y,pos.z);
         //set to impossible position to initialize a rebuild
-        this.oldFlooredPos = new Vector3i(0,-10,0);
+        this.oldFlooredPos.set(0,-10,0);
         this.inertia.set(inertia.x, inertia.y, inertia.z);
         this.mesh = mesh;
         this.timer = (float)Math.random()*2f;
@@ -30,8 +30,20 @@ public class ParticleObject {
     public ParticleObject(double x, double y, double z, Vector3f inertia, Mesh mesh, int key){
         this.pos.set(x,y,z);
         //set to impossible position to initialize a rebuild
-        this.oldFlooredPos = new Vector3i(0,-10,0);
+        this.oldFlooredPos.set(0,-10,0);
         this.inertia.set(inertia.x, inertia.y, inertia.z);
+        this.mesh = mesh;
+        this.timer = (float)Math.random()*2f;
+        this.key = key;
+        this.light = 15f;
+        this.lightUpdateTimer = 0;
+    }
+
+    public ParticleObject(double x, double y, double z, float inertiaX, float inertiaY, float inertiaZ, Mesh mesh, int key){
+        this.pos.set(x,y,z);
+        //set to impossible position to initialize a rebuild
+        this.oldFlooredPos.set(0,-10,0);
+        this.inertia.set(inertiaX, inertiaY, inertiaZ);
         this.mesh = mesh;
         this.timer = (float)Math.random()*2f;
         this.key = key;
