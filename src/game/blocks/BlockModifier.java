2312914a55038e8ef21a2022df665f601057391c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-12-27 18:25:01 -0500

    Rework BlockModifier interface to be data oriented

--- src/game/blocks/BlockModifier.java
+++ src/game/blocks/BlockModifier.java
@@ -1,18 +1,18 @@
 package game.blocks;
 
-import org.joml.Vector3d;
 import org.joml.Vector3i;
 
 public interface BlockModifier {
-    default public void onDig(Vector3d pos) {
+
+    default public void onDig(double posX, double posY, double posZ) {
 
     }
 
-    default public void onPlace(Vector3i pos) {
+    default public void onPlace(int posX, int posY, int posZ) {
 
     }
 
-    default public void onRightClick(Vector3i pos){
+    default public void onRightClick(int posX, int posY, int posZ){
 
     }
 }
