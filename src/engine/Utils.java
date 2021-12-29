47bc476b8f24be5c653fc7eea5dea092ad355785
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-12-28 20:42:47 -0500

    Assert non-null in loadResource

--- src/engine/Utils.java
+++ src/engine/Utils.java
@@ -25,9 +25,11 @@ public class Utils {
 
     public static String loadResource(String fileName) throws Exception{
         String result;
-        try(InputStream in = Utils.class.getResourceAsStream(fileName);
-            Scanner scanner = new Scanner(in, StandardCharsets.UTF_8.name())){
-            result = scanner.useDelimiter("\\A").next();
+        try(InputStream in = Utils.class.getResourceAsStream(fileName)) {
+            assert in != null;
+            try(Scanner scanner = new Scanner(in, StandardCharsets.UTF_8.name())){
+                result = scanner.useDelimiter("\\A").next();
+            }
         }
         return result;
     }
