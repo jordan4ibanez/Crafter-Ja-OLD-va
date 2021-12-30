10a2fa8c7fb9b9f3d2be8bde1d0ef8e1ff86359c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-12-29 23:43:29 -0500

    Add in a disk handler which controls the sqlite thread

---
+++ src/engine/disk/SQLiteDiskHandler.java
@@ -0,0 +1,39 @@
+package engine.disk;
+
+//this handles the thread object and tells it what to do
+public class SQLiteDiskHandler {
+
+    //null, asleep, non-existent, etc
+    private static SQLiteDiskAccessThread sqLiteDiskAccessThread;
+
+    //this mirrors the object's call
+    public static void connectWorldDataBase(String worldName){
+        sqLiteDiskAccessThread = new SQLiteDiskAccessThread();
+        sqLiteDiskAccessThread.connectWorldDataBase(worldName);
+
+        sqLiteDiskAccessThread.start();
+    }
+
+    //closes the world's database, kills the thread, removes the object pointer
+    public static void closeWorldDataBase(){
+        sqLiteDiskAccessThread.stop();
+
+        //nullify, this thread will stop itself
+        //this avoids an accidental pointer mix up
+        sqLiteDiskAccessThread = null;
+    }
+
+
+    public static void loadChunk(int x, int z){
+        sqLiteDiskAccessThread.loadChunk(x,z);
+    }
+
+    public static void saveChunk(int x, int z){
+        sqLiteDiskAccessThread.saveChunk(x,z);
+    }
+
+
+
+
+
+}
