93b94f4f0a6ab79b6cdfe83e2098183db3cd5b81
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-02 04:50:00 -0500

    Data orient the Texture class

--- src/engine/graphics/Texture.java
+++ src/engine/graphics/Texture.java
@@ -1,5 +1,6 @@
 package engine.graphics;
 
+import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
 import org.lwjgl.system.MemoryStack;
 
 import java.nio.ByteBuffer;
@@ -8,15 +9,16 @@ import java.nio.IntBuffer;
 import static org.lwjgl.opengl.GL44.*;
 import static org.lwjgl.stb.STBImage.*;
 
-public class Texture {
+final public class Texture {
+    private static final Int2IntOpenHashMap width  = new Int2IntOpenHashMap();
+    private static final Int2IntOpenHashMap height = new Int2IntOpenHashMap();
 
-    private final int id;
-    private final int width;
-    private final int height;
 
-    public Texture(String fileName) throws Exception {
+    public static int createTexture(String fileName) {
 
         final ByteBuffer buf;
+        int thisWidth;
+        int thisHeight;
 
         // Load Texture file
         try (MemoryStack stack = MemoryStack.stackPush()) {
@@ -26,18 +28,30 @@ public class Texture {
 
             buf = stbi_load(fileName, w, h, channels, 4);
             if (buf == null) {
-                throw new Exception("Image file [" + fileName  + "] not loaded: " + stbi_failure_reason());
+                System.out.println("Image file [" + fileName  + "] not loaded: " + stbi_failure_reason());
+                //throw new Exception("Image file [" + fileName  + "] not loaded: " + stbi_failure_reason());
             }
 
-            width = w.get();
-            height = h.get();
+            thisWidth = w.get();
+            thisHeight = h.get();
         }
-        this.id = createTexture(buf);
-        stbi_image_free(buf);
+
+        int thisID = createTexture(buf, thisWidth, thisHeight);
+
+        if (buf != null) {
+            width.put(thisID, thisWidth);
+            height.put(thisID, thisHeight);
+
+            stbi_image_free(buf);
+        }
+
+        return thisID;
     }
 
-    public Texture(ByteBuffer imageBuffer) throws Exception {
+    public Texture(ByteBuffer imageBuffer) {
         final ByteBuffer buf;
+        int thisWidth;
+        int thisHeight;
 
         // Load Texture file
         try (MemoryStack stack = MemoryStack.stackPush()) {
@@ -47,19 +61,27 @@ public class Texture {
 
             buf = stbi_load_from_memory(imageBuffer, w, h, channels, 4);
             if (buf == null) {
-                throw new Exception("Image file not loaded: " + stbi_failure_reason());
+                System.out.println("Image file not loaded: " + stbi_failure_reason());
+                //throw new Exception("Image file not loaded: " + stbi_failure_reason());
             }
 
-            width = w.get();
-            height = h.get();
+            thisWidth = w.get();
+            thisHeight = h.get();
         }
 
-        this.id = createTexture(buf);
+        int thisID = createTexture(buf, thisWidth,thisHeight);
 
-        stbi_image_free(buf);
+        if (buf != null) {
+            width.put(thisID, thisWidth);
+            height.put(thisID, thisHeight);
+
+            stbi_image_free(buf);
+        }
+
+
     }
 
-    private int createTexture(ByteBuffer buf) {
+    private static int createTexture(ByteBuffer buf, int thisWidth, int thisHeight) {
         // Create a new OpenGL texture
         int textureId = glGenTextures();
         // Bind the texture
@@ -72,27 +94,25 @@ public class Texture {
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
         // Upload the texture data
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0,
-                GL_RGBA, GL_UNSIGNED_BYTE, buf);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, thisWidth, thisHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, buf);
         // Generate Mip Map
 //        glGenerateMipmap(GL_TEXTURE_2D);
 
         return textureId;
     }
 
-    public int getWidth() {
-        return this.width;
+    public int getWidth(int gettingID) {
+        return width.get(gettingID);
     }
 
-    public int getHeight() {
-        return this.height;
+    public int getHeight(int gettingID) {
+        return height.get(gettingID);
     }
 
-    public int getId() {
-        return id;
-    }
+    public void cleanup(int gettingID) {
+        glDeleteTextures(gettingID);
 
-    public void cleanup() {
-        glDeleteTextures(id);
+        width.remove(gettingID);
+        height.remove(gettingID);
     }
 }
