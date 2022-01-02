5768ba3fa722987174b045c3905e6814df3d0ce5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-02 11:07:33 -0500

    Update GUIObject.java

--- src/engine/gui/GUIObject.java
+++ src/engine/gui/GUIObject.java
@@ -4,6 +4,7 @@ import engine.graphics.Mesh;
 import org.joml.Vector2d;
 import org.joml.Vector2f;
 
+import static engine.graphics.Mesh.cleanUpMesh;
 import static engine.gui.TextHandling.*;
 
 public class GUIObject {
@@ -15,7 +16,7 @@ public class GUIObject {
      */
 
     public byte type;
-    public Mesh textMesh;
+    public int textMesh;
     public Vector2d pos;
     public Vector2f buttonScale = new Vector2f();
     public boolean selected;
@@ -95,7 +96,7 @@ public class GUIObject {
 
     //updater for text input box
     public void updateInputBoxText(String text){
-        this.textMesh.cleanUp(false);
+        cleanUpMesh(this.textMesh,false);
         this.textMesh = createText(text, 1,1,1);
     }
 
@@ -105,18 +106,18 @@ public class GUIObject {
 
     //update statically scaled button text
     public void updateTextCenteredFixed(String text){
-        this.textMesh.cleanUp(false);
+        cleanUpMesh(this.textMesh,false);
         this.textMesh = createTextCenteredWithShadow(text, 1,1,1);
     }
 
     public void updateTextStandard(String text){
-        this.textMesh.cleanUp(false);
+        cleanUpMesh(this.textMesh,false);
         this.textMesh = createTextWithShadow(text, 1,1,1);
     }
 
     //updating dynamically scaled button text
     public void updateTextDynamic(String text){
-        this.textMesh.cleanUp(false);
+        cleanUpMesh(this.textMesh,false);
         this.textMesh = createTextCentered(text, 1,1,1);
         float totalLengthReal = 0;
         //pre-poll the actual length
