7e4b05b73803ccf0e7f9f8699351a5cf648a0810
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-04 11:05:46 -0500

    Data orient sound listener

--- src/engine/sound/SoundListener.java
+++ src/engine/sound/SoundListener.java
@@ -5,39 +5,29 @@ import org.joml.Vector3f;
 
 import static org.lwjgl.openal.AL10.*;
 
-public class SoundListener {
+final public class SoundListener {
 
-    public SoundListener() {
-        this( new Vector3d() );
-    }
-
-    public SoundListener(Vector3d position){
-        alListener3f(AL_POSITION, (float)position.x, (float)position.y, (float)position.z);
+    //0,0,0 is initial position
+    public static void createSoundListener(){
+        alListener3f(AL_POSITION, 0,0,0);
         alListener3f(AL_VELOCITY, 0,0,0);
     }
 
-    public void setSpeed(Vector3f speed){
-        alListener3f(AL_VELOCITY, speed.x, speed.y, speed.z);
+    public static void setSoundSpeed(float speedX, float speedY, float speedZ){
+        alListener3f(AL_VELOCITY, speedX, speedY, speedZ);
     }
 
-    public void setPosition(Vector3d position){
-        alListener3f(AL_POSITION, (float)position.x,(float)position.y,(float)position.z);
+    //auto casted, sound imprecision is less noticeable
+    public static void setSoundPosition(double positionX, double positionY, double positionZ){
+        alListener3f(AL_POSITION, (float)positionX,(float)positionY,(float)positionZ);
     }
 
-    public void setOrientation(Vector3d at, Vector3d up){
-        float[] data = new float[6];
-
-        data[0] = (float)at.x;
-        data[1] = (float)at.y;
-        data[2] = (float)at.z;
-
-        data[3] = (float)up.x;
-        data[4] = (float)up.y;
-        data[5] = (float)up.z;
-
+    public static void setSoundOrientation(double atX, double atY, double atZ, double upX, double upY, double upZ){
+        float[] data = new float[]{
+            (float)atX, (float)atY, (float)atZ,
+            (float)upX, (float)upY, (float)upZ
+        };
         alListenerfv(AL_ORIENTATION, data);
-
-
     }
 
 
