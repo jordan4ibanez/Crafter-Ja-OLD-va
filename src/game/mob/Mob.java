090bf48aef90be867ef222c58e6098145b88ecca
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-12 14:11:58 -0500

    Optimize mob memory access

--- src/game/mob/Mob.java
+++ src/game/mob/Mob.java
@@ -2,6 +2,7 @@ package game.mob;
 
 
 import org.joml.Vector3d;
+import org.joml.Vector3f;
 import org.joml.Vector3i;
 
 import java.util.ArrayDeque;
@@ -21,20 +22,32 @@ final public class Mob {
 
     private static final Deque<Integer> deletionQueue = new ArrayDeque<>();
 
+    private static final Vector3i currentFlooredPos = new Vector3i();
+
     public static void mobsOnTick(){
 
         double delta = getDelta();
 
+        //mob table key is int
         for (int thisMob : getMobKeys()){
 
             //this is the mob definition ID
-            int thisMobID = getMobID(thisMob);
-
-            float thisMobWidth = getMobWidth(thisMobID);
-            float thisMobHeight = getMobHeight(thisMobID);
+            int thisMobDefinitionID = getMobID(thisMob);
 
             //this is an object pointer
             Vector3d thisMobPos = getMobPos(thisMob);
+            Vector3i thisMobOldFlooredPos = getMobOldFlooredPos(thisMob);
+
+            //these are immutable primitives - they need to be set after they are modified
+            float thisMobTimer = getMobTimer(thisMob);
+            float thisMobHurtTimer = getMobHurtTimer(thisMob);
+            float thisMobLightTimer = getMobLightTimer(thisMob);
+            float thisMobDeathRotation = getMobDeathRotation(thisMob);
+            float thisMobWidth = getMobWidth(thisMobDefinitionID);
+            float thisMobHeight = getMobHeight(thisMobDefinitionID);
+
+            byte thisMobHealth = getMobHealth(thisMob);
+
 
             //only collision detect if alive
             if (getMobHealth(thisMob) > 0) {
@@ -43,73 +56,86 @@ final public class Mob {
             }
 
             //interface consumes object - no need for re-assignment to vars
-            //todo: this needs a complete reimplementation 
-            getMobInterface(thisMobID).onTick(thisMob);
+            //todo: this needs a complete reimplementation
+            getMobInterface(thisMobDefinitionID).onTick(thisMob);
 
-            if (thisMob.pos.y < 0){
-                deletionQueue.add(thisMob.globalID);
+            if (thisMobPos.y < 0){
+                deletionQueue.add(thisMob);
                 continue;
             }
 
             //mob dying animation
-            if (thisMob.health <= 0 && thisMob.deathRotation < 90){
-                thisMob.deathRotation += delta * 300f;
-                if (thisMob.deathRotation >= 90){
-                    thisMob.deathRotation = 90;
-                    thisMob.timer = 0f;
+            if (thisMobHealth <= 0 && thisMobDeathRotation < 90){
+                thisMobDeathRotation += delta * 300f;
+                if (thisMobDeathRotation >= 90){
+                    thisMobDeathRotation = 90;
+                    thisMobTimer = 0f;
                 }
+
+                setMobDeathRotation(thisMob, thisMobDeathRotation);
             }
 
-            Vector3i currentFlooredPos = new Vector3i((int)Math.floor(thisMob.pos.x), (int)Math.floor(thisMob.pos.y), (int)Math.floor(thisMob.pos.z));
+            currentFlooredPos.set((int)Math.floor(thisMobPos.x), (int)Math.floor(thisMobPos.y), (int)Math.floor(thisMobPos.z));
 
             //poll local light every quarter second
-            if (thisMob.lightTimer >= 0.25f || !currentFlooredPos.equals(thisMob.oldFlooredPos)){
-                thisMob.light = getLight(currentFlooredPos.x, currentFlooredPos.y, currentFlooredPos.z);
-                thisMob.lightTimer = 0f;
+            if (thisMobLightTimer >= 0.25f || !currentFlooredPos.equals(thisMobOldFlooredPos)){
+                setMobLight(thisMob, getLight(currentFlooredPos.x, currentFlooredPos.y, currentFlooredPos.z));
+                setMobLightTimer(thisMob, 0f);
             }
 
-            if (thisMob.health <= 0 && thisMob.timer >= 0.5f && thisMob.deathRotation == 90){
-                deletionQueue.add(thisMob.globalID);
+            if (thisMobHealth <= 0 && thisMobTimer >= 0.5f && thisMobDeathRotation == 90){
+                deletionQueue.add(thisMob);
             }
 
             //count down hurt timer
-            if(thisMob.hurtTimer > 0f && thisMob.health > 0){ //don't reset when dead
-                thisMob.hurtTimer -= delta;
-                if (thisMob.hurtTimer <= 0){
-                    thisMob.hurtTimer = 0;
-                    thisMob.hurtAdder = 0; //reset red coloring
+            if(thisMobHurtTimer > 0f && thisMobHealth > 0){ //don't reset when dead
+                thisMobHurtTimer -= delta;
+                if (thisMobHurtTimer <= 0){
+                    thisMobHurtTimer = 0;
+                    setMobHurtAdder(thisMob, (byte) 0);
                 }
+
+                setMobHurtTimer(thisMob, thisMobHurtTimer);
             }
 
-            thisMob.oldFlooredPos.set(currentFlooredPos);
-            thisMob.oldPos.set(thisMob.pos);
+            getMobOldFlooredPos(thisMob).set(currentFlooredPos);
+            getMobOldPos(thisMob).set(thisMob);
         }
 
         while (!deletionQueue.isEmpty()){
             int thisMobGlobalID = deletionQueue.pop();
             //System.out.println("mob " + thisMobGlobalID + " was deleted!");
-            mobs.remove(thisMobGlobalID);
-            globalMobs--;
+            deleteMob(thisMobGlobalID);
         }
     }
 
 
-    public static void punchMob(MobObject thisMob){
-        if (thisMob.hurtTimer <= 0 && thisMob.health > 0) {
-            thisMob.health -= 1;
+    public static void punchMob(int thisMob){
+
+        float thisMobHurtTimer = getMobHurtTimer(thisMob);
+        byte thisMobHealth = getMobHealth(thisMob);
+
+        if (thisMobHurtTimer <= 0 && thisMobHealth > 0) {
+            thisMobHealth -= 1;
+
             //System.out.println("the mobs health is: " + thisMob.health);
-            playSound(getHurtSound(thisMob.ID),(float)thisMob.pos.x, (float)thisMob.pos.y, (float)thisMob.pos.z, true);
+
+            Vector3d thisMobPos = getMobPos(thisMob);
+
+            playSound(getMobHurtSound(thisMob),(float)thisMobPos.x, (float)thisMobPos.y, (float)thisMobPos.z, true);
             //play this after in case ID changes
-            MobInterface thisInterface = getMobInterface(thisMob.ID);
+            MobInterface thisInterface = getMobInterface(thisMob);
             if (thisInterface != null){
                 thisInterface.onPunch(thisMob);
             }
 
-            if (thisMob.onGround) {
-                thisMob.inertia.y = 7;
+            if (getIfMobOnGround(thisMob)) {
+                getMobInertia(thisMob).y = 7;
             }
-            thisMob.hurtAdder = 15;
-            thisMob.hurtTimer = 0.5f;
+
+            setMobHealth(thisMob, thisMobHealth);
+            setMobHurtAdder(thisMob, (byte) 15);
+            setMobHurtTimer(thisMob, 0.5f);
         }
     }
 }
