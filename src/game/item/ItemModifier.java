f7e06d589a61ce0575436cd02249ccf7bdf00732
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-16 23:19:16 -0500

    Make item modifier intake primitives

--- src/game/item/ItemModifier.java
+++ src/game/item/ItemModifier.java
@@ -3,7 +3,7 @@ package game.item;
 import org.joml.Vector3i;
 
 public interface ItemModifier {
-    default public void onPlace(Vector3i pos, Vector3i pointedThingAbove) {
+    default public void onPlace(int posX, int posY, int posZ, int pointedThingAboveX, int pointedThingAboveY, int pointedThingAboveZ) {
         System.out.println("placing interface worked");
     }
 
