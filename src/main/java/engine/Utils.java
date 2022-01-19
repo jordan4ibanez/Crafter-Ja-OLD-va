946e6801033562f8c287c0edd4a1438551994b66
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-18 23:05:03 -0500

    Add assertion to possible null pointer in Utils

--- src/main/java/engine/Utils.java
+++ src/main/java/engine/Utils.java
@@ -53,6 +53,9 @@ public class Utils {
 
     public static float[] listToArray(List<Float> list) {
         int size = list != null ? list.size() : 0;
+        
+        assert list != null;
+        
         float[] floatArr = new float[size];
         for (int i = 0; i < size; i++) {
             floatArr[i] = list.get(i);
