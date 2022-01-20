df30753787c87f7441ae1e40e6b53ac2e886e963
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-20 07:49:24 -0500

    Implement EntityInterface into Entity

--- src/main/java/game/entity/Entity.java
+++ src/main/java/game/entity/Entity.java
@@ -1,7 +1,8 @@
 package game.entity;
 
-public class Entity {
+public class Entity implements EntityInterface{
     public void sayHi(){
         System.out.println("hi there");
+        this.sayBye();
     }
 }
