f8b174d91aeb485d252c4406d20fd0e5a95facee
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-20 08:25:22 -0500

    Migrate off glfw time for system compatibility

--- src/main/java/engine/time/Delta.java
+++ src/main/java/engine/time/Delta.java
@@ -1,19 +1,19 @@
 package engine.time;
 
-import static org.lwjgl.glfw.GLFW.glfwGetTime;
+import static java.lang.System.nanoTime;
 
 public class Delta {
 
-    private double lastLoopTime = getTime();
+    private double lastLoopTime = nanoTime();
     private double delta;
 
     private double getTime() {
-        return glfwGetTime();
+        return nanoTime();
     }
 
     public void calculateDelta() {
         double time = getTime();
-        delta =  (time - lastLoopTime);
+        delta =  (time - lastLoopTime) / 1_000_000;
         lastLoopTime = time;
     }
 
