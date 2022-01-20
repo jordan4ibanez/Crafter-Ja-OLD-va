b5ef784b88ee00eff4d1c820d9f287768f825200
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-20 06:29:12 -0500

    Update BlockModifier.java

--- src/main/java/game/blocks/BlockModifier.java
+++ src/main/java/game/blocks/BlockModifier.java
@@ -2,15 +2,15 @@ package game.blocks;
 
 public interface BlockModifier {
 
-    default public void onDig(double posX, double posY, double posZ) {
+    default void onDig(double posX, double posY, double posZ) {
 
     }
 
-    default public void onPlace(int posX, int posY, int posZ) {
+    default void onPlace(int posX, int posY, int posZ) {
 
     }
 
-    default public void onRightClick(int posX, int posY, int posZ){
+    default void onRightClick(int posX, int posY, int posZ){
 
     }
 }
