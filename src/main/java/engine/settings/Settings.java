9f57d3bbe2e81af1668385c64e268ee968a706ba
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-21 07:14:16 -0500

    Rebuild Settings.java

--- src/main/java/engine/settings/Settings.java
+++ src/main/java/engine/settings/Settings.java
@@ -1,231 +1,151 @@
 package engine.settings;
 
-import engine.Window.setVSync;
-import engine.disk.Disk.loadSettingsFromDisk;
-import engine.disk.Disk.saveSettingsToDisk;
-import game.chunk.Chunk.generateNewChunks;
-import org.lwjgl.glfw.GLFW.*;
+import engine.Window;
+import engine.disk.Disk;
+
+import static org.lwjgl.glfw.GLFW.*;
 
 public class Settings {
 
-    //todo: fix crashing when players mess with Settings.conf
+    private final Window window;
+    private final Disk disk;
+    private final SettingsObject settingsObject;
+    private boolean debugInfo; //assigned false
 
-    private SettingsObject settings;
+    public Settings(Disk disk, Window window){
+        this.disk = disk;
+        this.window = window;
 
-    //false for production (debugInfo)
-    private boolean debugInfo = false;
-    private boolean vSync;
-    private boolean graphicsMode; //true = fancy, false = fast
-    private byte chunkLoading;
-    private int renderDistance;
-
-    private int keyForward;
-    private int keyBack;
-    private int keyLeft;
-    private int keyRight;
-    private int keySneak;
-    private int keyDrop;
-    private int keyJump;
-    private int keyInventory;
-
-    public void loadSettings(){
-
-        SettingsObject loadedSettings = loadSettingsFromDisk();
+        SettingsObject loadedSettings = disk.loadSettingsFromDisk();
 
         if (loadedSettings == null) {
-            //default if no settings set
-
-            //instantiate new object
-            settings = new SettingsObject();
-
-            vSync = true;
-            settings.vSync = true;
-
-            graphicsMode = true;
-            settings.graphicsMode = true;
-
-            renderDistance = 5;
-            settings.renderDistance = 5;
-
-            //lower slower, higher faster
-            chunkLoading = 2;
-            settings.chunkLoading = 2;
-
-            keyForward = GLFW_KEY_W;
-            settings.keyForward = GLFW_KEY_W;
-
-            keyBack = GLFW_KEY_S;
-            settings.keyBack = GLFW_KEY_S;
-
-            keyLeft = GLFW_KEY_A;
-            settings.keyLeft = GLFW_KEY_A;
-
-            keyRight = GLFW_KEY_D;
-            settings.keyRight = GLFW_KEY_D;
-
-            keySneak = GLFW_KEY_LEFT_SHIFT;
-            settings.keySneak = GLFW_KEY_LEFT_SHIFT;
-
-            keyDrop = GLFW_KEY_Q;
-            settings.keyDrop = GLFW_KEY_Q;
-
-            keyJump = GLFW_KEY_SPACE;
-            settings.keyJump = GLFW_KEY_SPACE;
-
-            keyInventory = GLFW_KEY_E;
-            settings.keyInventory = GLFW_KEY_E;
+            this.settingsObject = new SettingsObject();
+            this.settingsObject.vSync = true;
+            this.settingsObject.graphicsMode = true;
+            this.settingsObject.renderDistance = 5;
+            this.settingsObject.keyForward = GLFW_KEY_W;
+            this.settingsObject.keyBack = GLFW_KEY_S;
+            this.settingsObject.keyLeft = GLFW_KEY_A;
+            this.settingsObject.keyRight = GLFW_KEY_D;
+            this.settingsObject.keySneak = GLFW_KEY_LEFT_SHIFT;
+            this.settingsObject.keyDrop = GLFW_KEY_Q;
+            this.settingsObject.keyJump = GLFW_KEY_SPACE;
+            this.settingsObject.keyInventory = GLFW_KEY_E;
 
             //save default values
-            saveSettingsToDisk(settings);
+            disk.saveSettingsToDisk(settingsObject);
 
         } else {
-
             //dump new settings in
-            settings = loadedSettings;
-
-            vSync = settings.vSync;
-            graphicsMode = settings.graphicsMode;
-            renderDistance = settings.renderDistance;
-            chunkLoading = settings.chunkLoading;
-
-            keyForward = settings.keyForward;
-            keyBack = settings.keyBack;
-            keyLeft = settings.keyLeft;
-            keyRight = settings.keyRight;
-            keySneak = settings.keySneak;
-            keyDrop = settings.keyDrop;
-            keyJump = settings.keyJump;
-            keyInventory = settings.keyInventory;
+            this.settingsObject = loadedSettings;
+            //manually update vsync as it's loaded in
+            window.setVSync(this.settingsObject.vSync);
         }
     }
 
     //debug info
     public boolean getDebugInfo(){
-        return debugInfo;
+        return this.debugInfo;
     }
+
     public void setDebugInfo(boolean truth){
         debugInfo = truth;
-    }
-    public void invertDebugInfoBoolean(){ //todo: remove this crap
-        debugInfo = !debugInfo;
     }
 
     //render distance
     public int getRenderDistance(){
-        return renderDistance;
+        return this.settingsObject.renderDistance;
     }
+
     public void setRenderDistance(int newRenderDistance, boolean inGame){
-        renderDistance = newRenderDistance;
-        settings.renderDistance = newRenderDistance;
+        this.settingsObject.renderDistance = newRenderDistance;
         if (inGame) {
             generateNewChunks();
         }
     }
 
-    //vsync
     public void setSettingsVsync(boolean truth){
-        vSync = truth;
-        settings.vSync = truth;
-        setVSync(truth);
+        this.settingsObject.vSync = truth;
+        window.setVSync(truth);
     }
+
     public boolean getSettingsVsync(){
-        return vSync;
+        return this.settingsObject.vSync;
     }
 
-    //lazy chunk loading
-    public void setSettingsChunkLoad(byte truth){
-        chunkLoading = truth;
-        settings.chunkLoading = truth;
-    }
-    public byte getSettingsChunkLoad(){
-        return chunkLoading;
-    }
-
-    //graphics mode
     public void setGraphicsMode(boolean newGraphicsMode){
-        graphicsMode = newGraphicsMode;
-        settings.graphicsMode = newGraphicsMode;
+        this.settingsObject.graphicsMode = newGraphicsMode;
     }
+
     public boolean getGraphicsMode(){
-        return graphicsMode;
+        return this.settingsObject.graphicsMode;
     }
 
-    //input keys
-
-    //forward
     public void setKeyForward(int newKey){
-        keyForward = newKey;
-        settings.keyForward = newKey;
+        this.settingsObject.keyForward = newKey;
     }
+
     public int getKeyForward(){
-        return keyForward;
+        return this.settingsObject.keyForward;
     }
 
-    //back
     public void setKeyBack(int newKey){
-        keyBack = newKey;
-        settings.keyBack = newKey;
+        this.settingsObject.keyBack = newKey;
     }
+
     public int getKeyBack(){
-        return keyBack;
+        return this.settingsObject.keyBack;
     }
 
-    //left
     public void setKeyLeft(int newKey){
-        keyLeft = newKey;
-        settings.keyLeft = newKey;
+        this.settingsObject.keyLeft = newKey;
     }
+
     public int getKeyLeft(){
-        return keyLeft;
+        return this.settingsObject.keyLeft;
     }
 
-    //right
     public void setKeyRight(int newKey){
-        keyRight = newKey;
-        settings.keyRight = newKey;
+        this.settingsObject.keyRight = newKey;
     }
+
     public int getKeyRight(){
-        return keyRight;
+        return this.settingsObject.keyRight;
     }
 
-    //sneak
     public void setKeySneak(int newKey){
-        keySneak = newKey;
-        settings.keySneak = newKey;
+        this.settingsObject.keySneak = newKey;
     }
+
     public int getKeySneak(){
-        return keySneak;
+        return this.settingsObject.keySneak;
     }
 
-    //drop
     public void setKeyDrop(int newKey){
-        keyDrop = newKey;
-        settings.keyDrop = newKey;
+        this.settingsObject.keyDrop = newKey;
     }
+
     public int getKeyDrop(){
-        return keyDrop;
+        return this.settingsObject.keyDrop;
     }
 
-    //jump
     public void setKeyJump(int newKey){
-        keyJump = newKey;
-        settings.keyJump = newKey;
+        this.settingsObject.keyJump = newKey;
     }
+
     public int getKeyJump(){
-        return keyJump;
+        return this.settingsObject.keyJump;
     }
 
-    //inventory
     public void setKeyInventory(int newKey){
-        keyInventory = newKey;
-        settings.keyInventory = newKey;
-    }
-    public int getKeyInventory(){
-        return keyInventory;
+        this.settingsObject.keyInventory = newKey;
     }
 
-    //settings saving
+    public int getKeyInventory(){
+        return this.settingsObject.keyInventory;
+    }
+
     public void saveSettings(){
-        saveSettingsToDisk(settings);
+        disk.saveSettingsToDisk(settingsObject);
     }
 }
