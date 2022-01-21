e0235352e81fc3cec2373c5969386bfb912eb8eb
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-21 02:40:59 -0500

    Make Settings an object

--- src/main/java/engine/settings/Settings.java
+++ src/main/java/engine/settings/Settings.java
@@ -1,34 +1,34 @@
 package engine.settings;
 
-import static engine.Window.setVSync;
-import static engine.disk.Disk.loadSettingsFromDisk;
-import static engine.disk.Disk.saveSettingsToDisk;
-import static game.chunk.Chunk.generateNewChunks;
-import static org.lwjgl.glfw.GLFW.*;
+import engine.Window.setVSync;
+import engine.disk.Disk.loadSettingsFromDisk;
+import engine.disk.Disk.saveSettingsToDisk;
+import game.chunk.Chunk.generateNewChunks;
+import org.lwjgl.glfw.GLFW.*;
 
 public class Settings {
 
     //todo: fix crashing when players mess with Settings.conf
 
-    private static SettingsObject settings;
+    private SettingsObject settings;
 
     //false for production (debugInfo)
-    private static boolean debugInfo = false;
-    private static boolean vSync;
-    private static boolean graphicsMode; //true = fancy, false = fast
-    private static byte chunkLoading;
-    private static int renderDistance;
+    private boolean debugInfo = false;
+    private boolean vSync;
+    private boolean graphicsMode; //true = fancy, false = fast
+    private byte chunkLoading;
+    private int renderDistance;
 
-    private static int keyForward;
-    private static int keyBack;
-    private static int keyLeft;
-    private static int keyRight;
-    private static int keySneak;
-    private static int keyDrop;
-    private static int keyJump;
-    private static int keyInventory;
+    private int keyForward;
+    private int keyBack;
+    private int keyLeft;
+    private int keyRight;
+    private int keySneak;
+    private int keyDrop;
+    private int keyJump;
+    private int keyInventory;
 
-    public static void loadSettings(){
+    public void loadSettings(){
 
         SettingsObject loadedSettings = loadSettingsFromDisk();
 
@@ -100,21 +100,21 @@ public class Settings {
     }
 
     //debug info
-    public static boolean getDebugInfo(){
+    public boolean getDebugInfo(){
         return debugInfo;
     }
-    public static void setDebugInfo(boolean truth){
+    public void setDebugInfo(boolean truth){
         debugInfo = truth;
     }
-    public static void invertDebugInfoBoolean(){ //todo: remove this crap
+    public void invertDebugInfoBoolean(){ //todo: remove this crap
         debugInfo = !debugInfo;
     }
 
     //render distance
-    public static int getRenderDistance(){
+    public int getRenderDistance(){
         return renderDistance;
     }
-    public static void setRenderDistance(int newRenderDistance, boolean inGame){
+    public void setRenderDistance(int newRenderDistance, boolean inGame){
         renderDistance = newRenderDistance;
         settings.renderDistance = newRenderDistance;
         if (inGame) {
@@ -123,109 +123,109 @@ public class Settings {
     }
 
     //vsync
-    public static void setSettingsVsync(boolean truth){
+    public void setSettingsVsync(boolean truth){
         vSync = truth;
         settings.vSync = truth;
         setVSync(truth);
     }
-    public static boolean getSettingsVsync(){
+    public boolean getSettingsVsync(){
         return vSync;
     }
 
     //lazy chunk loading
-    public static void setSettingsChunkLoad(byte truth){
+    public void setSettingsChunkLoad(byte truth){
         chunkLoading = truth;
         settings.chunkLoading = truth;
     }
-    public static byte getSettingsChunkLoad(){
+    public byte getSettingsChunkLoad(){
         return chunkLoading;
     }
 
     //graphics mode
-    public static void setGraphicsMode(boolean newGraphicsMode){
+    public void setGraphicsMode(boolean newGraphicsMode){
         graphicsMode = newGraphicsMode;
         settings.graphicsMode = newGraphicsMode;
     }
-    public static boolean getGraphicsMode(){
+    public boolean getGraphicsMode(){
         return graphicsMode;
     }
 
     //input keys
 
     //forward
-    public static void setKeyForward(int newKey){
+    public void setKeyForward(int newKey){
         keyForward = newKey;
         settings.keyForward = newKey;
     }
-    public static int getKeyForward(){
+    public int getKeyForward(){
         return keyForward;
     }
 
     //back
-    public static void setKeyBack(int newKey){
+    public void setKeyBack(int newKey){
         keyBack = newKey;
         settings.keyBack = newKey;
     }
-    public static int getKeyBack(){
+    public int getKeyBack(){
         return keyBack;
     }
 
     //left
-    public static void setKeyLeft(int newKey){
+    public void setKeyLeft(int newKey){
         keyLeft = newKey;
         settings.keyLeft = newKey;
     }
-    public static int getKeyLeft(){
+    public int getKeyLeft(){
         return keyLeft;
     }
 
     //right
-    public static void setKeyRight(int newKey){
+    public void setKeyRight(int newKey){
         keyRight = newKey;
         settings.keyRight = newKey;
     }
-    public static int getKeyRight(){
+    public int getKeyRight(){
         return keyRight;
     }
 
     //sneak
-    public static void setKeySneak(int newKey){
+    public void setKeySneak(int newKey){
         keySneak = newKey;
         settings.keySneak = newKey;
     }
-    public static int getKeySneak(){
+    public int getKeySneak(){
         return keySneak;
     }
 
     //drop
-    public static void setKeyDrop(int newKey){
+    public void setKeyDrop(int newKey){
         keyDrop = newKey;
         settings.keyDrop = newKey;
     }
-    public static int getKeyDrop(){
+    public int getKeyDrop(){
         return keyDrop;
     }
 
     //jump
-    public static void setKeyJump(int newKey){
+    public void setKeyJump(int newKey){
         keyJump = newKey;
         settings.keyJump = newKey;
     }
-    public static int getKeyJump(){
+    public int getKeyJump(){
         return keyJump;
     }
 
     //inventory
-    public static void setKeyInventory(int newKey){
+    public void setKeyInventory(int newKey){
         keyInventory = newKey;
         settings.keyInventory = newKey;
     }
-    public static int getKeyInventory(){
+    public int getKeyInventory(){
         return keyInventory;
     }
 
     //settings saving
-    public static void saveSettings(){
+    public void saveSettings(){
         saveSettingsToDisk(settings);
     }
 }
