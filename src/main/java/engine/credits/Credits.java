84b0b04282c36c73478098f77766d5acf2756ff0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-20 23:19:09 -0500

    Object orient Credits

--- src/main/java/engine/credits/Credits.java
+++ src/main/java/engine/credits/Credits.java
@@ -9,13 +9,21 @@ import static engine.gui.TextHandling.createTextCenteredWithShadow;
 public class Credits {
 
     //all the stuff for the credits are stored here
-    private static final Mesh[] creditParts = new Mesh[19];
+    private final Mesh[] creditParts = new Mesh[19];
 
-    public static Mesh[] getCreditParts(){
+    public Credits(){
+        this.initializeCredits();
+    }
+
+    public Mesh[] getCreditMeshArray(){
         return creditParts;
     }
 
-    public static void initializeCredits() {
+    public Mesh getCreditMesh(int ID){
+        return creditParts[ID];
+    }
+
+    private void initializeCredits() {
         creditParts[0] = createTextCenteredWithShadow("Inspired by:", 1,1,1);
         creditParts[1] = create2DMesh(8.4375f,1f,"textures/minetest_logo.png");
         creditParts[2] = create2DMesh(9.34782608696f,1f,"textures/mineclone2.png");
