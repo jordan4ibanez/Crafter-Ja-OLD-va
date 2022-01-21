da91bf32809b1a0bbc6d651caa48f775d75a7a2b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-21 07:54:34 -0500

    Update ChunkSavingObject.java

--- src/main/java/engine/disk/ChunkSavingObject.java
+++ src/main/java/engine/disk/ChunkSavingObject.java
@@ -1,10 +1,20 @@
 package engine.disk;
 
+import org.joml.Vector2i;
+import org.joml.Vector3d;
+
 public class ChunkSavingObject {
-    public int x;
-    public int z;
-    public byte [] b;
-    public byte[] r;
-    public byte[] l;
-    public byte[] h;
+    public Vector2i pos;
+    public byte[] block;
+    public byte[] rotation;
+    public byte[] light;
+    public byte[] heightMap;
+
+    public ChunkSavingObject(Vector2i pos, byte[] block, byte[] rotation, byte[] light, byte[] heightMap){
+        this.pos = pos;
+        this.block = block;
+        this.rotation = rotation;
+        this.light = light;
+        this.heightMap = heightMap;
+    }
 }
