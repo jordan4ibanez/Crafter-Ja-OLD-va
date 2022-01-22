00c21c8476c18b62fdb176201dcfe38506722cfb
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-22 07:46:01 -0500

    Add chunk container to BlockModifier

--- src/main/java/game/blocks/BlockModifier.java
+++ src/main/java/game/blocks/BlockModifier.java
@@ -1,16 +1,18 @@
 package game.blocks;
 
+import game.chunk.Chunk;
+
 public interface BlockModifier {
 
-    default void onDig(double posX, double posY, double posZ) {
+    default void onDig(double posX, double posY, double posZ, Chunk chunk) {
 
     }
 
-    default void onPlace(int posX, int posY, int posZ) {
+    default void onPlace(int posX, int posY, int posZ, Chunk chunk) {
 
     }
 
-    default void onRightClick(int posX, int posY, int posZ){
+    default void onRightClick(int posX, int posY, int posZ, Chunk chunk){
 
     }
 }
