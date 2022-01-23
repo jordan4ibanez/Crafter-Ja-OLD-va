37e6ee06a0293a66cba3f8f97e246848af9c4ae0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-23 06:34:32 -0500

    Turn inventory object back into an object

--- src/main/java/game/crafting/InventoryObject.java
+++ src/main/java/game/crafting/InventoryObject.java
@@ -1,141 +1,98 @@
 package game.crafting;
 
-import it.unimi.dsi.fastutil.objects.Object2DoubleOpenHashMap;
-import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
-import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
-
-import engine.network.Networking.getIfMultiplayer;
+import org.joml.Vector2d;
+import org.joml.Vector2i;
 
 final public class InventoryObject {
 
-    private final Object2ObjectOpenHashMap<String, String[][]> inventory = new Object2ObjectOpenHashMap<>();
-    private final Object2ObjectOpenHashMap<String, int[][]> count = new Object2ObjectOpenHashMap<>();
+    private final String name;
 
-    //selection is initially off the grid
-    private final Object2IntOpenHashMap<String> selectionX = new Object2IntOpenHashMap<>();
-    private final Object2IntOpenHashMap<String> selectionY = new Object2IntOpenHashMap<>();
+    private final String[][] inventory;
+    private final int[][] count;
 
-    private final Object2DoubleOpenHashMap<String> posX = new Object2DoubleOpenHashMap<>();
-    private final Object2DoubleOpenHashMap<String> posY = new Object2DoubleOpenHashMap<>();
+    private final Vector2i selection = new Vector2i(-1,-1);
+    private final Vector2d pos;
+    private final Vector2i size;
 
-    private final Object2IntOpenHashMap<String> sizeX = new Object2IntOpenHashMap<>();
-    private final Object2IntOpenHashMap<String> sizeY = new Object2IntOpenHashMap<>();
+    private final boolean mainInventory;
 
-    //private final boolean mainInventory;
-
-    public void createInventory(String newName, int newSizeX, int newSizeY, double newPosX, double newPosY, boolean isMainInventory){
-        inventory.put(newName, new String[newSizeY][newSizeX]);
-        count.put(newName, new int[newSizeY][newSizeX]);
-
-        selectionX.put(newName, -1);
-        selectionY.put(newName, -1);
-
-        sizeX.put(newName, newSizeX);
-        sizeY.put(newName, newSizeY);
-
-        posX.put(newName, newPosX);
-        posY.put(newName, newPosY);
+    public InventoryObject(String name, Vector2i size, Vector2d pos, boolean mainInventory){
+        this.name = name;
+        this.inventory = new String[size.y][size.x];
+        this.count =  new int[size.y][size.x];
+        this.size = new Vector2i();
+        this.pos = new Vector2d(pos);
+        this.mainInventory = mainInventory;
     }
 
-
-    //external modification
-    public void setInventoryItem(String name, int x, int y, String newItem, int newCount){
-        inventory.get(name)[y][x] = newItem;
-        count.get(name)[y][x] = newCount;
+    public void setItem(int x, int y, String newItem, int newCount){
+        inventory[y][x] = newItem;
+        count[y][x] = newCount;
     }
 
-    //mutable
-    public String getItemInInventory(String name, int x, int y){
-        return inventory.get(name)[y][x];
+    public String getItem(String name, int x, int y){
+        return inventory[y][x];
     }
 
-    //immutable
-    public int getInventoryCount(String name, int x, int y){
-        return count.get(name)[y][x];
+    public int getCount(String name, int x, int y){
+        return count[y][x];
     }
 
-    //external modification
-    public void setInventoryCount(String name, int x, int y, int newCount){
-        count.get(name)[y][x] = newCount;
+    public void setCount(String name, int x, int y, int newCount){
+        count[y][x] = newCount;
     }
 
-    //mutable
     public String[][] getInventoryAsArray(String name){
-        return inventory.get(name);
+        return inventory;
     }
 
-    //mutable
-    public int[][] getInventoryCountAsArray(String name){
-        return count.get(name);
+    public int[][] getCountAsArray(String name){
+        return count;
     }
 
-    //external modification
-    public void deleteInventoryItem(String name, int x, int y){
-        inventory.get(name)[y][x] = null;
-        count.get(name)[y][x] = 0;
+    public void deleteItem(String name, int x, int y){
+        inventory[y][x] = null;
+        count[y][x] = 0;
     }
 
-    //external modification
-    public void clearOutInventory(String name){
-        String[][] thisInventory = inventory.get(name);
-        int[][] thisCount = count.get(name);
-        for (int y = 0; y < thisInventory.length; y++) {
-            for (int x = 0; x < thisInventory[0].length; x++) {
-                thisInventory[y][x] = null;
-                thisCount[y][x] = 0;
+    public void clear(String name){
+        for (int y = 0; y < size.y; y++) {
+            for (int x = 0; x < size.x; x++) {
+                inventory[y][x] = null;
+                count[y][x] = 0;
             }
         }
     }
 
-    //external modification
-    public void setInventorySelection(String name, int x, int y){
-        selectionX.put(name, x);
-        selectionY.put(name, y);
+    public void setSelection(String name, int x, int y){
+        selection.set(x,y);
     }
 
-    //immutable
-    public int getInventorySelectionX(String name){
-        return selectionX.getInt(name);
+
+    public Vector2d getPos(){
+        return pos;
     }
 
-    //immutable
-    public int getInventorySelectionY(String name){
-        return selectionY.getInt(name);
+    public Vector2i getSize(){
+        return size;
     }
 
-    //immutable
-    public double getInventoryPosX(String name){
-        return posX.getDouble(name);
+    public String getName(){
+        return name;
     }
 
-    //immutable
-    public double getInventoryPosY(String name){
-        return posY.getDouble(name);
-    }
-
-    //immutable
-    public int getInventorySizeX(String name){
-        return sizeX.getInt(name);
-    }
-    //immutable
-    public int getInventorySizeY(String name){
-        return sizeY.getInt(name);
-    }
-
-    //a simple public tool to add items to an inventory
-    public boolean addToInventory(String name, String newItemName){
-        String[][] thisInventory = inventory.get(name);
-        int[][] thisCount = count.get(name);
-
+    public boolean addItem(String name, String item){
         //check whole inventory
-        for (int y = 0; y < thisInventory.length; y++) {
-            for (int x = 0; x < thisInventory[0].length; x++) {
-                if (thisInventory[y][x] != null && thisInventory[y][x].equals(newItemName) && thisCount[y][x] < 64){
-                    tickUpInventoryStack(name, x,y);
+        for (int y = 0; y < size.y; y++) {
+            for (int x = 0; x < size.x; x++) {
+                if (inventory[y][x] != null && inventory[y][x].equals(item) && count[y][x] < 64){
+                    tickUpStack(name, x,y);
+                    /*
                     if (getIfMultiplayer()){
                         System.out.println("oh nuuuu gotta fix this");
                         //sendServerUpdatedInventory();
                     }
+                     */
                     return true;
                 }
             }
@@ -143,13 +100,15 @@ final public class InventoryObject {
         //failed to find one, create new stack
         for (int y = 0; y < 4; y++) {
             for (int x = 0; x < 9; x++) {
-                if (thisInventory[y][x] == null){
-                    thisInventory[y][x] = newItemName;
-                    thisCount[y][x] = 1;
+                if (inventory[y][x] == null){
+                    inventory[y][x] = item;
+                    count[y][x] = 1;
+                    /*
                     if (getIfMultiplayer()){
                         System.out.println("oh nuuuu gotta fix this");
                         //sendServerUpdatedInventory();
                     }
+                     */
                     return true;
                 }
             }
@@ -157,24 +116,21 @@ final public class InventoryObject {
         return false;
     }
 
-    //external modification
-    public void removeItemFromInventory(String name, int x, int y){
-        String[][] thisInventory = inventory.get(name);
-        int[][] thisCount = count.get(name);
-        if (thisCount[y][x] > 0) {
-            thisCount[y][x]--;
-            if (thisCount[y][x] <= 0) {
-                thisInventory[y][x] = null;
-                thisCount[y][x] = 0;
+    public void removeItem(String name, int x, int y){
+        if (count[y][x] > 0) {
+            count[y][x]--;
+            if (count[y][x] <= 0) {
+                inventory[y][x] = null;
+                count[y][x] = 0;
             }
         } else {
-            thisInventory[y][x] = null;
+            inventory[y][x] = null;
         }
     }
 
     //internal
-    private void tickUpInventoryStack(String name, int x, int y){
-        count.get(name)[y][x]++;
+    private void tickUpStack(String name, int x, int y){
+        count[y][x]++;
     }
 
 }
