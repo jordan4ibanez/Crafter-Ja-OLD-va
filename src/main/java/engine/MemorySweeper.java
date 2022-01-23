31c71a866191fa370734d1ce93494e8f4fafaf86
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-23 14:13:47 -0500

    Remove dead imports

--- src/main/java/engine/MemorySweeper.java
+++ src/main/java/engine/MemorySweeper.java
@@ -1,13 +1,6 @@
 package engine;
 
 import static game.chat.Chat.cleanChatMemory;
-import static game.chunk.Chunk.cleanChunkDataMemory;
-import static game.clouds.Cloud.cleanCloudMemory;
-import static game.crafting.Inventory.cleanInventoryMemory;
-import static game.falling.FallingEntity.cleanFallingEntities;
-import static game.entity.item.ItemEntity.cleanItemMemory;
-import static game.entity.particle.Particle.cleanParticleMemory;
-import static game.player.OtherPlayers.cleanOtherPLayerMemory;
 
 public class MemorySweeper {
     public static void cleanMemory(){
