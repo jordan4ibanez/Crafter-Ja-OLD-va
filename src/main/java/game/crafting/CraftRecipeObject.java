2be3cc9c33eff0a2155b2e9a83decfd2b79b0a97
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-23 09:18:50 -0500

    Update CraftRecipeObject.java

--- src/main/java/game/crafting/CraftRecipeObject.java
+++ src/main/java/game/crafting/CraftRecipeObject.java
@@ -1,5 +1,28 @@
 package game.crafting;
 
-public class CraftRecipeObject {
+import it.unimi.dsi.fastutil.ints.Int2IntArrayMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectArrayMap;
 
+public class CraftRecipeObject {
+    private final String[][] recipe;
+    private final String output;
+    private final int amount;
+
+    public CraftRecipeObject(String[][] recipe, String output, int amount){
+        this.recipe = recipe;
+        this.output = output;
+        this.amount = amount;
+    }
+
+    public String[][] getRecipe(){
+        return recipe;
+    }
+
+    public String getOutput(){
+        return output;
+    }
+
+    public int getAmount(){
+        return amount;
+    }
 }
