8cbfd43a75bf42a32f2e31b0e77d20e94e05dcf0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-24 12:07:59 -0500

    Refactor ItemModifier to be OOP

--- src/main/java/game/entity/item/ItemModifier.java
+++ src/main/java/game/entity/item/ItemModifier.java
@@ -4,8 +4,11 @@ import game.blocks.BlockDefinitionContainer;
 import org.joml.Vector3i;
 
 public interface ItemModifier {
-    default void onPlace(Vector3i pos, Vector3i pointedThingAbove, BlockDefinitionContainer definitionContainer) {
+    default void onPlace(ItemEntity itemEntity){
         System.out.println("placing interface worked");
+    }
+    default void onPickUp(ItemEntity itemEntity){
+        System.out.println("I'm picked up woooo");
     }
 
 }
