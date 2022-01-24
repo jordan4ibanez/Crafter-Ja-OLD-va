1ec16f7467ec79a222b87de63a7f0022ac22a446
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-24 12:04:21 -0500

    Add in entity interface methods

--- src/main/java/game/entity/EntityInterface.java
+++ src/main/java/game/entity/EntityInterface.java
@@ -1,7 +1,27 @@
 package game.entity;
 
 public interface EntityInterface {
-    default void sayBye(){
-        System.out.println("Welp see you later");
+    default void onSpawn(Entity entity){
+        System.out.println("Welp I'm here now!");
+    }
+
+    default void onTick(Entity entity){
+        System.out.println("tick tock!");
+    }
+
+    default void onHeal(Entity entity){
+        System.out.println("I'm feeling a bit better!");
+    }
+
+    default void onHurt(Entity entity){
+        System.out.println("Ouch!");
+    }
+
+    default void onDeath(Entity entity){
+        System.out.println("Oof I am le dead!");
+    }
+
+    default void sayBye(Entity entity){
+        System.out.println("Welp see you later!");
     }
 }
