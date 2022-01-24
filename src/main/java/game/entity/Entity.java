945e6f9977cd956b49f557fb10c64fecc3e4a322
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-24 12:41:11 -0500

    Add support for mobs and items in Entity Abstraction

--- src/main/java/game/entity/Entity.java
+++ src/main/java/game/entity/Entity.java
@@ -1,11 +1,12 @@
 package game.entity;
 
+import engine.time.Delta;
 import org.joml.Vector3d;
 import org.joml.Vector3f;
 
 import java.util.UUID;
 
-public class Entity implements EntityInterface{
+public abstract class Entity implements EntityInterface{
 
     //master pointer
     private final EntityContainer entityContainer;
@@ -13,21 +14,51 @@ public class Entity implements EntityInterface{
     private final UUID uuid = UUID.randomUUID();
     private final Vector3d pos = new Vector3d();
     private final Vector3f inertia = new Vector3f();
+    private float timer = 0;
+    private final boolean item;
+    private final boolean mob;
 
-    public Entity(EntityContainer entityContainer, Vector3d pos, Vector3f inertia){
+    public Entity(EntityContainer entityContainer, Vector3d pos, Vector3f inertia, boolean item, boolean mob){
         this.entityContainer = entityContainer;
         this.entityContainer.add(this);
 
         this.pos.set(pos);
         this.inertia.set(inertia);
+
+        this.item = item;
+        this.mob = mob;
+    }
+
+    public boolean isItem(){
+        return this.item;
+    }
+
+    public boolean isMob(){
+        return this.mob;
+    }
+
+    public float getTimer(){
+        return this.timer;
+    }
+
+    public void setTimer(float timer){
+        this.timer = timer;
     }
 
     public Vector3d getPos(){
         return this.pos;
     }
 
+    public void setPos(Vector3d pos){
+        this.pos.set(pos);
+    }
+
     public Vector3f getInertia(){
         return this.inertia;
+    }
+
+    public void setInertia(Vector3f inertia){
+        this.inertia.set(inertia);
     }
 
     public void delete(){
@@ -41,4 +72,5 @@ public class Entity implements EntityInterface{
     }
 
 
+    public abstract void onTick(Entity entity, Delta delta);
 }
