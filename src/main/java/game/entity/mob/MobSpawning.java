41ec9310137faa63195199a8691ce6b9e629e376
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-25 09:53:23 -0500

    Re-prototype mob spawning

--- src/main/java/game/entity/mob/MobSpawning.java
+++ src/main/java/game/entity/mob/MobSpawning.java
@@ -1,43 +1,50 @@
 package game.entity.mob;
 
+import engine.time.Delta;
+import game.chunk.Chunk;
+import game.entity.EntityContainer;
+import game.player.Player;
 import org.joml.Vector3d;
+import org.joml.Vector3f;
 
-import engine.FancyMath.randomByte;
-import engine.FancyMath.randomIntFromMinToMaxNegativePositive;
-import engine.time.Delta.getDelta;
-import game.chunk.Chunk.getMobSpawnYPos;
-
-import game.player.Player.getPlayerPos;
+import java.util.Random;
 
 public class MobSpawning {
-    private final int spawnLimit = 20;
     private double spawnTimer = 1f;
-    private final float spawnGoal = 3f; //every 3 seconds
+    private final Random random = new Random();
 
-    public void runSpawningAlgorithm(){
-        if (MobObject.getNumberOfMobs() >= spawnLimit){
-            return;
-        }
+    public void runSpawningAlgorithm(Player player, Chunk chunk, Delta delta, MobMeshBuilder mobMeshBuilder, EntityContainer entityContainer){
+        int spawnLimit = 20;
         //having this not count up allows a minor cool down
-        spawnTimer += getDelta();
+        spawnTimer += delta.getDelta();
 
+        //every 3 seconds
+        float spawnGoal = 3f;
         if (spawnTimer >= spawnGoal){
             //CHANGE THIS TO CHECK FOR PLAYERS POSITION WHEN TRANSLATING TO MULTIPLAYER
-            trySpawn(getPlayerPos());
+            trySpawn(player.getPlayerPos(), chunk, mobMeshBuilder, entityContainer);
             spawnTimer = 0;
         }
     }
 
 
     //this is a square distance, acceptable is 24-56 blocks away from the player
-    private void trySpawn(Vector3d pos){
+    private void trySpawn(Vector3d pos, Chunk chunk, MobMeshBuilder mobMeshBuilder, EntityContainer entityContainer){
         //a 2d calculation
-        int x = (int)pos.x + randomIntFromMinToMaxNegativePositive(24,56);
-        int z = (int)pos.z + randomIntFromMinToMaxNegativePositive(24,56);
-        int yPos = getMobSpawnYPos(x,z);
+        int x = (int)pos.x + randomInt(24,56);
+        int z = (int)pos.z + randomInt(24,56);
+        int yPos = chunk.getMobSpawnYPos(x,z);
         if (yPos > 0){
-            MobObject.spawnMob(randomByte((byte) 9), x, yPos, z, 0, 0, 0);
+            //randomByte(9){
+            //}
+            //spawnMob(randomByte((byte) 9), x, yPos, z, 0, 0, 0);
+            new Zombie(mobMeshBuilder, entityContainer, new Vector3d(x, yPos, z), new Vector3f(0,0,0));
         }
     }
+    private final int[] dirArray = new int[]{-1,1};
 
+    public int randomInt(int min, int max){
+        int x = min + random.nextInt(max - min + 1);
+        return x * dirArray[random.nextInt(2)];
+    }
 }
