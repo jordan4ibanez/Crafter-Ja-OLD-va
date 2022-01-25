10dfebeea2fa11e92f6087708d77e6cd8a30e595
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-25 12:25:44 -0500

    Dump Disk into sqlite handler

--- src/main/java/engine/disk/SQLiteDiskHandler.java
+++ src/main/java/engine/disk/SQLiteDiskHandler.java
@@ -1,5 +1,7 @@
 package engine.disk;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import engine.settings.SettingsObject;
 import game.chunk.BiomeGenerator;
 import game.chunk.Chunk;
 import game.crafting.Inventory;
@@ -7,6 +9,13 @@ import game.crafting.InventoryLogic;
 import game.player.Player;
 import org.joml.Vector2i;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
 public class SQLiteDiskHandler {
@@ -127,5 +136,115 @@ public class SQLiteDiskHandler {
         this.generatingChunks.add(new Vector2i(pos));
     }
 
+    private SQLiteDiskHandler sqLiteDiskHandler;
+
+    public Disk(){
+
+    }
+
+    public void setSqLiteDiskHandler(SQLiteDiskHandler sqLiteDiskHandler){
+        if (this.sqLiteDiskHandler == null){
+            this.sqLiteDiskHandler = sqLiteDiskHandler;
+        }
+    }
+
+    private byte currentActiveWorld = 1; //failsafe
+
+    public void updateSaveQueueCurrentActiveWorld(byte newWorld){
+        currentActiveWorld = newWorld;
+    }
+
+    public byte getCurrentActiveWorld(){
+        return currentActiveWorld;
+    }
+
+    //this is the main entry point for disk access to the world from the main menu
+    public void setCurrentActiveWorld(byte newWorld){
+        currentActiveWorld = newWorld;
+        updateSaveQueueCurrentActiveWorld(newWorld);
+        createAlphaWorldFolder();
+        //eh no where to really put this so just gonna stick it here
+        sqLiteDiskHandler.connectWorldDataBase("world" + currentActiveWorld);
+        System.out.println("CURRENT WORLD IS: " + newWorld);
+    }
+
+    //https://stackoverflow.com/a/24734290
+    //https://stackoverflow.com/a/3758880
+    public String worldSize(byte world) {
+        long bytes = 0;
+
+        Path thisWorld = Paths.get("Worlds/world" + world);
+
+        if (!Files.isDirectory(Paths.get("Worlds/world" + world))){
+            return "";
+        }
+
+        try {
+            bytes = Files.walk(thisWorld).mapToLong( p -> p.toFile().length() ).sum();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (-1000 < bytes && bytes < 1000) {
+            return bytes + " B";
+        }
+        CharacterIterator ci = new StringCharacterIterator("kMGTPE");
+        while (bytes <= -999_950 || bytes >= 999_950) {
+            bytes /= 1000;
+            ci.next();
+        }
+        return " (" + String.format("%.1f %cB", bytes / 1000.0, ci.current()) + ")";
+    }
+
+    public void createWorldsDir(){
+        try {
+            Files.createDirectories(Paths.get("Worlds"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createAlphaWorldFolder(){
+        try {
+            Files.createDirectories(Paths.get("Worlds/world" + currentActiveWorld));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateWorldsPathToAvoidCrash(){
+        createWorldsDir();
+        createAlphaWorldFolder();
+    }
+
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+
+    public SettingsObject loadSettingsFromDisk(){
+        File file = new File("Settings.conf");
+
+        if (!file.canRead()){
+            return null;
+        }
+
+        SettingsObject settings = null;
+
+        try {
+            settings = objectMapper.readValue(file, SettingsObject.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return settings;
+    }
+
+    public void saveSettingsToDisk(SettingsObject settingsObject){
+        try {
+            objectMapper.writeValue(new File("Settings.conf"), settingsObject);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
