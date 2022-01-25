660048de7cd2e8651933641705cefee64f086d7c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-25 12:32:38 -0500

    Convert PrimitiveChunkObject to record

--- src/main/java/engine/disk/PrimitiveChunkObject.java
+++ src/main/java/engine/disk/PrimitiveChunkObject.java
@@ -2,18 +2,4 @@ package engine.disk;
 
 import org.joml.Vector2i;
 
-public class PrimitiveChunkObject {
-    public Vector2i pos;
-    public byte[] block;
-    public byte[] rotation;
-    public byte[] light;
-    public byte[] heightMap;
-
-    public PrimitiveChunkObject(Vector2i pos, byte[] block, byte[] rotation, byte[] light, byte[] heightMap){
-        this.pos = pos;
-        this.block = block;
-        this.rotation = rotation;
-        this.light = light;
-        this.heightMap = heightMap;
-    }
-}
+public record PrimitiveChunkObject(Vector2i pos, byte[] block, byte[] rotation, byte[] light, byte[] heightMap) {}
