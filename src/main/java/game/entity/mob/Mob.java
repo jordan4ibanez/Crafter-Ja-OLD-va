e410de8d8633ecf33cb603e81a74975be4ee7fbe
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-25 07:50:19 -0500

    Move mob utility code into mob

--- src/main/java/game/entity/mob/Mob.java
+++ src/main/java/game/entity/mob/Mob.java
@@ -5,6 +5,7 @@ import game.chunk.Chunk;
 import game.entity.Entity;
 import game.entity.EntityContainer;
 import game.player.Player;
+import org.joml.Math;
 import org.joml.Vector3d;
 import org.joml.Vector3f;
 
@@ -17,10 +18,17 @@ public class Mob extends Entity {
     private int health;
     private int hurtAdder = 0;
 
-    public Mob(EntityContainer entityContainer, String name, Vector3d pos, Vector3f inertia, float width, float height, int health) {
+    private final MobInterface mobInterface;
+
+    public Mob(EntityContainer entityContainer, String name, MobInterface mobInterface, Vector3d pos, Vector3f inertia, float width, float height, int health) {
         super(entityContainer, pos, inertia, width, height, false, true, false);
+        this.mobInterface = mobInterface;
         this.name = name;
         this.health = health;
+    }
+
+    public MobInterface getMobInterface(){
+        return mobInterface;
     }
 
     public String getName(){
@@ -92,5 +100,114 @@ public class Mob extends Entity {
         this.health -= damage;
         this.hurtAdder = 15;
         this.hurtTimer = 0.5f;
+    }
+
+    //mob utility code
+    private final Vector3d headPos = new Vector3d();
+    private final Vector3d headTurn = new Vector3d();
+    private final Vector3d adjustedHeadPos = new Vector3d();
+
+    public void doHeadCode(Mob mob){
+
+        //this is a pointer object
+        Vector3d thisMobPos = mob.getPos();
+
+        //yet another pointer object
+        Vector3f[] thisMobBodyOffsets = mob.getBodyOffsets();
+
+        //look another pointer object
+        Vector3f[] thisMobBodyRotations = MobObject.getMobBodyRotations(thisMob);
+
+        float thisMobSmoothRotation = MobObject.getMobSmoothRotation(thisMob);
+
+        float smoothToRad = Math.toRadians(thisMobSmoothRotation + 90f);
+
+        //silly head turning
+        headPos.set(thisMobPos.x, thisMobPos.y, thisMobPos.z);
+        adjustedHeadPos.set(Math.cos(-smoothToRad), 0,Math.sin(smoothToRad));
+        adjustedHeadPos.mul(thisMobBodyOffsets[0].z).add(0,thisMobBodyOffsets[0].y,0);
+        headPos.add(adjustedHeadPos);
+
+        //check if the mob can actual "see" the player
+        if (!getLineOfSight(headPos, getPlayerPosWithEyeHeight())){
+            return;
+        }
+
+        //this is debug code for creating a new mob
+        //createParticle(headPos.x, headPos.y, headPos.z, 0.f,0.f,0.f, (byte) 7); //debug
+
+        headTurn.set(getPlayerPosWithEyeHeight()).sub(headPos);
+        //headTurn.normalize();
+
+        float headYaw = (float) Math.toDegrees(Math.atan2(headTurn.z, headTurn.x)) + 90 - thisMobSmoothRotation;
+        float pitch = (float)Math.toDegrees(Math.atan2(Math.sqrt(headTurn.z * headTurn.z + headTurn.x * headTurn.x), headTurn.y) + (Math.PI * 1.5));
+
+        //correction of degrees overflow (-piToDegrees to piToDegrees) so it is workable
+        if (headYaw < -180) {
+            headYaw += 360;
+        } else if (headYaw > 180){
+            headYaw -= 360;
+        }
+
+        //a temporary reset, looks creepy
+        if (headYaw > 90 || headYaw < -90){
+            headYaw = 0;
+            pitch = 0;
+        }
+
+        //weird OOP application
+        thisMobBodyRotations[0].set(pitch,headYaw,0);
+    }
+
+
+    //todo: shortest distance
+    public void mobSmoothRotation(Mob mob, Delta delta){
+        double dtime = delta.getDelta();
+
+        float thisMobRotation = mob.getRotation(mob);
+        float thisMobSmoothRotation = MobObject.getMobSmoothRotation(mob);
+
+        float diff = thisMobRotation - thisMobSmoothRotation;
+
+        //correction of degrees overflow (-piToDegrees to piToDegrees) so it is workable
+        if (diff < -180) {
+            diff += 360;
+        } else if (diff > 180){
+            diff -= 360;
+        }
+
+        /*
+        this is basically brute force inversion to correct the yaw
+        addition and make the mob move to the shortest rotation
+        vector possible
+         */
+
+        if (Math.abs(diff) < dtime * 500f){
+            thisMobSmoothRotation = thisMobRotation;
+        } else {
+            if (Math.abs(diff) > 180) {
+                if (diff < 0) {
+                    thisMobSmoothRotation += dtime * 500f;
+                } else if (diff > 0) {
+                    thisMobSmoothRotation -= dtime * 500f;
+                }
+
+                //correction of degrees overflow (-piToDegrees to piToDegrees) so it is workable
+                if (thisMobSmoothRotation < -180) {
+                    thisMobSmoothRotation += 360;
+                } else if (thisMobSmoothRotation > 180) {
+                    thisMobSmoothRotation -= 360;
+                }
+
+            } else {
+                if (diff < 0) {
+                    thisMobSmoothRotation -= dtime * 500f;
+                } else if (diff > 0) {
+                    thisMobSmoothRotation += dtime * 500f;
+                }
+            }
+        }
+
+        MobObject.setMobSmoothRotation(mob, thisMobSmoothRotation);
     }
 }
