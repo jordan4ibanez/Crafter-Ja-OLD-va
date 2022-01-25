c54c2fddd21d26281529f78abb2345a73f4b6e2f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-25 10:36:37 -0500

    Rewrite SceneHandler

--- src/main/java/engine/scene/SceneHandler.java
+++ src/main/java/engine/scene/SceneHandler.java
@@ -1,5 +1,28 @@
 package engine.scene;
 
+import engine.Controls;
+import engine.MemorySweeper;
+import engine.Mouse;
+import engine.Window;
+import engine.disk.SQLiteDiskHandler;
+import engine.graphics.Camera;
+import engine.gui.GUILogic;
+import engine.sound.SoundAPI;
+import engine.time.Delta;
+import engine.time.TimeOfDay;
+import engine.time.Timer;
+import game.chunk.Chunk;
+import game.chunk.ChunkUpdateHandler;
+import game.clouds.Cloud;
+import game.crafting.InventoryLogic;
+import game.entity.EntityContainer;
+import game.entity.mob.MobMeshBuilder;
+import game.entity.mob.MobSpawning;
+import game.mainMenu.MainMenu;
+import game.player.Player;
+import org.joml.Vector3d;
+import org.joml.Vector3f;
+
 import java.io.IOException;
 
 public class SceneHandler {
@@ -8,6 +31,53 @@ public class SceneHandler {
     //2 was debug - now reserved for something else in the future if needed
     //3 multiplayer
 
+    private Window window;
+    private MainMenu mainMenu;
+    private Mouse mouse;
+    private Chunk chunk;
+    private MemorySweeper memorySweeper;
+    private InventoryLogic inventoryLogic;
+    private GUILogic guiLogic;
+    private Cloud cloud;
+    private Player player;
+    private Camera camera;
+    private Delta delta;
+    private Controls controls;
+    private SQLiteDiskHandler sqLiteDiskHandler;
+    private TimeOfDay timeOfDay;
+    private Timer timer;
+    private MobSpawning mobSpawning;
+    private MobMeshBuilder mobMeshBuilder;
+    private EntityContainer entityContainer;
+    private ChunkUpdateHandler chunkUpdateHandler;
+    private SoundAPI soundAPI;
+
+    public void passObjects(Window window, MainMenu mainMenu, Mouse mouse, Chunk chunk, MemorySweeper memorySweeper, InventoryLogic inventoryLogic, GUILogic guiLogic,
+                            Cloud cloud, Player player, Camera camera, Delta delta, Controls controls,SQLiteDiskHandler sqLiteDiskHandler, TimeOfDay timeOfDay,
+                            Timer timer, MobSpawning mobSpawning, MobMeshBuilder mobMeshBuilder, EntityContainer entityContainer, ChunkUpdateHandler chunkUpdateHandler,
+                            SoundAPI soundAPI){
+        this.window = window;
+        this.mainMenu = mainMenu;
+        this.mouse = mouse;
+        this.chunk = chunk;
+        this.memorySweeper = memorySweeper;
+        this.inventoryLogic = inventoryLogic;
+        this.guiLogic = guiLogic;
+        this.cloud = cloud;
+        this.player = player;
+        this.camera = camera;
+        this.delta = delta;
+        this.controls = controls;
+        this.sqLiteDiskHandler = sqLiteDiskHandler;
+        this.timeOfDay = timeOfDay;
+        this.timer = timer;
+        this.mobSpawning = mobSpawning;
+        this.mobMeshBuilder = mobMeshBuilder;
+        this.entityContainer = entityContainer;
+        this.chunkUpdateHandler = chunkUpdateHandler;
+        this.soundAPI = soundAPI;
+    }
+
     private byte currentScene = 0;
 
     public void setScene(byte newScene){
@@ -15,15 +85,15 @@ public class SceneHandler {
         //move the camera into position for the main menu
         if (newScene == 0){
 
-            setWindowClearColor(0,0,0,1);
-            selectTitleScreenText();
-            if (isMouseLocked()){
-                toggleMouseLock();
+            window.setWindowClearColor(0,0,0,1);
+            mainMenu.selectTitleScreenText();
+            if (mouse.isLocked()){
+                mouse.toggleMouseLock();
             }
 
             //don't save the server chunks
             if(currentScene == 1) {
-                globalFinalChunkSaveToDisk();
+                chunk.globalFinalChunkSaveToDisk();
             }
         }
 
@@ -31,28 +101,28 @@ public class SceneHandler {
         //generic handler - this goes after scene 0 check for global final save chunks to disk
 
         //this cleans the memory out
-        cleanMemory();
+        memorySweeper.cleanMemory();
 
         //gameplay
         if (newScene == 1){
-            setWindowClearColor(0.53f,0.81f,0.92f,0.f);
-            calculateHealthBarElements(); //todo move this into a loader for player file things
-            initialChunkPayload();
+            window.setWindowClearColor(0.53f,0.81f,0.92f,0.f);
+            guiLogic.calculateHealthBarElements(); //todo move this into a loader for player file things
+            chunk.initialChunkPayload();
             //generateRandomInventory();
             System.out.println("link the cloud position setter into the world initialization protocol");
-            setCloudPos(getPlayerCurrentChunkX(),getPlayerCurrentChunkZ());
-            generateCloudData();
+            cloud.setCloudPos(player.getPlayerCurrentChunk().x,player.getPlayerCurrentChunk().y);
+            cloud.generateCloudData();
         }
 
         //multiplayer
         if (newScene == 3){
-            setWindowClearColor(0.53f,0.81f,0.92f,0.f);
-            calculateHealthBarElements(); //todo move this into a network thing or something
-            if (!isMouseLocked()){
-                toggleMouseLock();
+            window.setWindowClearColor(0.53f,0.81f,0.92f,0.f);
+            guiLogic.calculateHealthBarElements(); //todo move this into a network thing or something
+            if (!mouse.isLocked()){
+                mouse.toggleMouseLock();
             }
-            initialChunkPayloadMultiplayer();
-            generateCloudData();
+            //chunk.initialChunkPayloadMultiplayer();
+            cloud.generateCloudData();
         }
 
         currentScene = newScene;
@@ -63,11 +133,11 @@ public class SceneHandler {
 
         //move the camera into position for the main menu
         if (currentScene == 0){
-            setCameraPosition(0,-8,0);
+            camera.setCameraPosition(new Vector3d(0,-8,0));
         }
 
 
-        while (!windowShouldClose()){
+        while (!window.shouldClose()){
             //if (getDebugInfo()) {
                 //turn this back on
                 //doRuntimeInfoUpdate();
@@ -82,7 +152,10 @@ public class SceneHandler {
 
     }
 
-    private void multiPlayerLoop() throws Exception {
+
+    private void multiPlayerLoop() {
+        System.out.println("Multiplayer is disabled until Netty is implemented");
+        /*
         if (!getIfConnected()){
             setScene((byte)0);
             setMenuPage((byte)7);
@@ -107,11 +180,12 @@ public class SceneHandler {
         updateMultiplayerWorldChunkLoader();
         updateCamera();
         renderGame();
-
+         */
     }
 
 
     private void multiPlayerUpdate() {
+        /*
         testPlayerDiggingAnimation();
         playerOnTick();
         pauseMenuOnTick();
@@ -119,66 +193,67 @@ public class SceneHandler {
         particlesOnStep();
         //rainDropsOnTick();
         sendPositionData();
+         */
     }
 
 
     private void mainMenuLoop() throws IOException {
-        setCameraPosition(0,-8,0);
-        setCameraRotation(0,0,0);
-        windowUpdate();
-        calculateDelta();
-        mainMenuInput();
-        doMainMenuLogic();
+        camera.setCameraPosition(new Vector3d(0,-8,0));
+        camera.setCameraRotation(new Vector3f(0,0,0));
+        window.windowUpdate();
+        delta.calculateDelta();
+        controls.mainMenuInput();
+        mainMenu.doMainMenuLogic(this.delta);
         renderMainMenu();
     }
 
     //main game loop
     private void gameLoop() throws Exception {
-        pollReceivingPlayerDataFromSQLiteThread();
-        calculateDelta();
+        sqLiteDiskHandler.poll();
+        delta.calculateDelta();
         //indexLight();
-        mouseInput();
-        tickUpTimeOfDay();
+        mouse.mouseInput();
+        timeOfDay.tickUpTimeOfDay(this.delta);
         //pollTimeOfDay(); //this needs to be in the main thread
-        makeCloudsMove();
-        countFPS();
+        cloud.makeCloudsMove();
+        timer.countFPS();
 
-        runSpawningAlgorithm();
+        mobSpawning.runSpawningAlgorithm(this.player,this.chunk,this.delta,this.mobMeshBuilder,this.entityContainer);
 
-        updateWorldChunkLoader();
+        player.updateWorldChunkLoader();
 
-        popChunkMeshQueue(); //this actually transmits the data from the other threads into main thread
+        chunkUpdateHandler.popChunkMeshQueue(); //this actually transmits the data from the other threads into main thread
 
-        updateListenerPosition();
+        soundAPI.updateListenerPos();
 
         //chunkUpdater();
 
-        globalChunkSaveToDisk();
+        chunk.globalChunkSaveToDisk();
 
-        gameInput();
+        controls.gameInput();
 
         gameUpdate();
 
-        updateCamera();
+        camera.updateCamera();
 
         renderGame();
 
-        windowUpdate();
+        window.windowUpdate();
 
-        processOldChunks();
+        chunk.processOldChunks();
     }
 
     private void gameUpdate(){
-        doChunksHoveringUpThing();
-        testPlayerDiggingAnimation();
-        playerOnTick();
-        itemsOnTick();
+        chunk.doChunksHoveringUpThing(this.delta);
+        //player.testPlayerDiggingAnimation();
+        player.playerOnTick();
+        //itemsOnTick();
         //onTNTStep();
-        pauseMenuOnTick();
-        inventoryMenuOnTick();
-        particlesOnStep();
-        fallingEntityOnStep();
+        guiLogic.pauseMenuOnTick();
+        //inventoryMenuOnTick();
+        //particlesOnStep();
+        //fallingEntityOnStep();
         //rainDropsOnTick();
-        mobsOnTick();
+        //mobsOnTick();
     }
 }
