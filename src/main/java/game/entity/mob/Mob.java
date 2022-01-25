7c0b66ab0d2e23cf5362f4a1de8bc62447842a1f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-25 07:51:27 -0500

    Update Mob.java

--- src/main/java/game/entity/mob/Mob.java
+++ src/main/java/game/entity/mob/Mob.java
@@ -91,8 +91,8 @@ public class Mob extends Entity {
             }
         }
 
-        mobSmoothRotation(thisMob);
-        doHeadCode(thisMob);
+        mobSmoothRotation(this, delta);
+        doHeadCode(this);
     }
 
 
