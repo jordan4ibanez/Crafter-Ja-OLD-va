3c5c614287dab38f5f3950e9147878a4812c648a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-01-25 07:19:48 -0500

    Reorganize Entity

--- src/main/java/game/entity/Entity.java
+++ src/main/java/game/entity/Entity.java
@@ -1,12 +1,7 @@
 package game.entity;
 
-import engine.sound.SoundAPI;
 import engine.time.Delta;
 import game.chunk.Chunk;
-import game.crafting.InventoryLogic;
-import game.entity.collision.Collision;
-import game.entity.mob.Mob;
-import game.player.Player;
 import org.joml.Vector3d;
 import org.joml.Vector3f;
 import org.joml.Vector3i;
@@ -15,55 +10,77 @@ import java.util.UUID;
 
 import static org.joml.Math.floor;
 
-public abstract class Entity implements EntityInterface{
+public abstract class Entity {
 
-    //master pointers
     private final EntityContainer entityContainer;
-    private final Chunk chunk;
+
+    private final Vector3d pos = new Vector3d();
+    private final Vector3i flooredPos = new Vector3i();
+    private final Vector3i oldFlooredPos = new Vector3i();
+
+    private final Vector3f inertia = new Vector3f();
 
     private final UUID uuid = UUID.randomUUID();
-    private final Vector3d pos = new Vector3d();
-    private final Vector3f inertia = new Vector3f();
+
     private float timer = 0;
+    private byte light = 15;
+    private float lightUpdateTimer = 0f;
+
+    private final float width;
+    private final float height;
+
     private final boolean item;
     private final boolean mob;
     private final boolean particle;
-    private byte light = 15;
-    private float lightUpdateTimer = 0f;
-    private final Vector3i flooredPos;
-    private final Vector3i oldFlooredPos;
 
+    public Entity(EntityContainer entityContainer, Vector3d pos, Vector3f inertia, float width, float height, boolean item, boolean mob, boolean particle){
 
-    public Entity(Chunk chunk, EntityContainer entityContainer, Vector3d pos, Vector3f inertia, boolean item, boolean mob, boolean particle){
-        this.chunk = chunk;
-        this.entityContainer = entityContainer;
-        this.entityContainer.add(this);
-
-        this.pos.set(pos);
-        this.inertia.set(inertia);
+        this.width = width;
+        this.height = height;
 
         this.item = item;
         this.mob = mob;
         this.particle = particle;
 
-        this.flooredPos = new Vector3i((int) floor(pos.x), (int) floor(pos.y), (int) floor(pos.z));
-        this.oldFlooredPos = new Vector3i((int) floor(pos.x), (int) floor(pos.y), (int) floor(pos.z));
+        this.entityContainer = entityContainer;
+        this.entityContainer.add(this);
+
+        this.pos.set(pos);
+        this.inertia.set(inertia);
+        this.flooredPos.set((int) floor(pos.x), (int) floor(pos.y), (int) floor(pos.z));
+        this.oldFlooredPos.set((int) floor(pos.x), (int) floor(pos.y), (int) floor(pos.z));
+    }
+
+    public float getWidth(){
+        return width;
+    }
+
+    public float getHeight(){
+        return height;
+    }
+
+    public void delete(){
+        entityContainer.remove(this);
     }
 
     public UUID getUuid(){
-        return this.uuid;
+        return uuid;
     }
 
     public boolean isItem(){
-        return this.item;
+        return item;
     }
 
     public boolean isMob(){
-        return this.mob;
+        return mob;
+    }
+
+    public boolean isParticle() {
+        return particle;
     }
 
     public float getTimer(){
-        return this.timer;
+        return timer;
     }
 
     public void setTimer(float timer){
@@ -71,7 +88,7 @@ public abstract class Entity implements EntityInterface{
     }
 
     public Vector3d getPos(){
-        return this.pos;
+        return pos;
     }
 
     public void setPos(Vector3d pos){
@@ -79,27 +96,14 @@ public abstract class Entity implements EntityInterface{
     }
 
     public Vector3f getInertia(){
-        return this.inertia;
+        return inertia;
     }
 
     public void setInertia(Vector3f inertia){
         this.inertia.set(inertia);
     }
 
-    public void delete(){
-        entityContainer.remove(this);
-    }
-
-
-    public void sayHi(){
-        System.out.println("hi there");
-        this.sayBye(this);
-    }
-
-
-    @Override
-    public void onTick(Entity entity, Delta delta, Player player) {
-        EntityInterface.super.onTick(entity, delta, player);
+    public void onTick(Chunk chunk, Delta delta) {
 
         lightUpdateTimer += delta.getDelta();
 
@@ -114,23 +118,5 @@ public abstract class Entity implements EntityInterface{
         }
 
         oldFlooredPos.set(flooredPos);
-    };
-
-    public abstract float getWidth();
-
-    public abstract float getHeight();
-
-    public abstract int getHealth();
-
-    public abstract int getHurtAdder();
-
-    public abstract void hurt(int damage);
-
-    public abstract void onTick(Entity entity, Player player, Delta delta);
-
-    public abstract void onTick(Entity entity, InventoryLogic inventoryLogic, Player player, Delta delta);
-
-    public abstract void onTick(Entity entity, SoundAPI soundAPI, InventoryLogic inventoryLogic, Player player, Delta delta);
-
-    public abstract void onTick(Collision collision, Entity entity, SoundAPI soundAPI, InventoryLogic inventoryLogic, Player player, Delta delta);
+    }
 }
